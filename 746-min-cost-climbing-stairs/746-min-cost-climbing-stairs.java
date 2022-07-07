45ad0991d591ee46c22ab11e5682a0c133fd4407
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-07-07 16:42:52 +0530

    Time: 1 ms (89.25%), Space: 43.5 MB (57.55%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,29 +1,23 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int a = minCost(cost,0,new HashMap<Integer,Integer>());
-        int b = minCost(cost,1,new HashMap<Integer,Integer>());
+        int[] dp = new int[cost.length+2];
+        Arrays.fill(dp,-1);
+        int a = minCost(cost,0,dp);
+        int b = minCost(cost,1,dp);
         return Math.min(a,b);
     }
-    
-    private int minCost(int[] cost,int currIdx,HashMap<Integer,Integer> hm){
-        if(currIdx == cost.length){
+    public int minCost(int[] cost,int currIdx,int[] dp){
+        if(currIdx == cost.length)
             return 0;
-        }
-        if(currIdx > cost.length){
+        if(currIdx > cost.length)
             return (int)1e9;
-        }
+        if(dp[currIdx] != -1)
+            return dp[currIdx];
         
-        int currKey = currIdx;
-        if(hm.containsKey(currKey)){
-            return hm.get(currKey);
-        }
+        int oneStep = cost[currIdx] + minCost(cost,currIdx+1,dp);
+        int twoStep = cost[currIdx] + minCost(cost,currIdx+2,dp);
         
-        int oneStep = cost[currIdx] + minCost(cost,currIdx+1,hm);
-        int twoStep = cost[currIdx] + minCost(cost,currIdx+2,hm);
-        
-        hm.put(currIdx,Math.min(oneStep,twoStep));
-        
-        return Math.min(oneStep,twoStep);
+        dp[currIdx] = Math.min(oneStep,twoStep);
+        return dp[currIdx];
     }
-    
 }
