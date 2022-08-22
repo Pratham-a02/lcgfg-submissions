d6db152b048c7887fe4e11d26b730e3fd6d91c3b
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-08-22 08:40:01 +0530

    Time: 2 ms (55.22%), Space: 41.2 MB (86.57%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,31 +1,30 @@
 class Solution {
-    
     public int maxProfit(int[] prices) {
-        return bestTime(prices,0,1,new HashMap<String,Integer>());
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        for(int[]d:dp){
+            Arrays.fill(d,-1);
+        }
+        return bestTime(0,prices,1,dp);
     }
-    
-    public int bestTime(int[] prices,int currDay,int canBuy,HashMap<String,Integer>hm){
-        if(currDay>=prices.length)
+    public int bestTime(int currDay,int[] prices,int canBuy,int[][] dp){
+        if(currDay >= prices.length){
             return 0;
-
+        }
         
-        String currKey = currDay + "_" + canBuy;
-        
-        if(hm.containsKey(currKey))
-            return hm.get(currKey);
+        if(dp[currDay][canBuy] != -1){
+            return dp[currDay][canBuy];
+        }
         
         if(canBuy == 1){
-            int idle = bestTime(prices,currDay+1,canBuy,hm);
-            int buy =  bestTime(prices,currDay+1,0,hm)-prices[currDay];    
-            
-            hm.put(currKey,Math.max(idle,buy));
-            return hm.get(currKey);
+            int idle = bestTime(currDay+1,prices,1,dp);
+            int buy =  -prices[currDay] + bestTime(currDay+1,prices,0,dp);
+            return dp[currDay][canBuy] = Math.max(idle,buy);
         }
         else{
-            int idle = bestTime(prices,currDay+1,canBuy,hm);
-            int sell = prices[currDay] + bestTime(prices,currDay+2,1,hm);
-            hm.put(currKey,Math.max(idle,sell));
-            return hm.get(currKey);
+            int idle = bestTime(currDay+1,prices,0,dp);
+            int sell = prices[currDay] + bestTime(currDay+2,prices,1,dp);
+            return dp[currDay][canBuy] = Math.max(idle,sell);
         }
     }
 }
