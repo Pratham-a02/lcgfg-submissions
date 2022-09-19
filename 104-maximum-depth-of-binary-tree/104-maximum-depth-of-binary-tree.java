41a68a5b06cb927250c12b663e3888b88cf1e7b6
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-09-19 21:37:27 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (15.12%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -18,9 +18,9 @@ class Solution {
         if(root == null){
             return 0;
         }
-        if(root.left == null && root.right == null){
-            return 1;
-        }
+        // if(root.left == null && root.right == null){
+        //     return 1;
+        // }
         
         int left = 1 + maxDepth(root.left);
         int right = 1 + maxDepth(root.right);
