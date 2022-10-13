766d4f6223181b1388815c942ab36bff19d9e7c2
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-10-13 06:49:21 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (71.78%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,13 +14,38 @@
  * }
  */
 class Solution {
-    public int countNodes(TreeNode root) {
+    public int countNodes(TreeNode root){
         if(root == null){
             return 0;
         }
-        int ls = countNodes(root.left);
-        int rs = countNodes(root.right);
-        
-        return ls + rs + 1;
+        // if(root.left == null && root.right == null){
+        //     return root.val;
+        // }
+        int leftHeight = findLeftHeight(root);
+        int rightHeight = findRightHeight(root);
+        if(leftHeight==rightHeight){
+            int ht = leftHeight+1;
+            return (1<<ht) -1;
+        }
+        else{
+            return countNodes(root.left) + countNodes(root.right) + 1;
+        }
     }
+    
+public int findLeftHeight(TreeNode root){
+        if(root == null || root.left == null){
+            return 0;
+        }
+        int height = 1 + findLeftHeight(root.left);
+        return height;
+    }
+    
+    public int findRightHeight(TreeNode root){
+        if(root == null || root.right == null){
+            return 0;
+        }
+        int height = 1 + findRightHeight(root.right);
+        return height;
+    }
+    
 }
