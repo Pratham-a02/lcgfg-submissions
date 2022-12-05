3c9e2736590fa6625844055c4c1dcf6c5c5a3c0f
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-12-05 21:02:49 +0530

    Added solution - LeetHub

---
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] nums = new int[n];
+            String[] S = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.minSwaps(nums);
+            System.out.println(ans);
+       }
+    }
+}
+// } Driver Code Ends
+class Solution{
+    
+    public int minSwaps(int nums[]){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            hm.put(nums[i],i);
+        }
+        
+        Arrays.sort(nums);
+        boolean[] vis = new boolean[nums.length];
+        int ans = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(vis[i] == true || hm.get(nums[i]) == i){
+                continue;
+            }
+            vis[i] = true;
+            int csize = 0;
+            int j = i;
+            
+            do{
+                j = hm.get(nums[j]);
+                vis[j] = true;
+                csize++;
+            }while(i!=j);
+            
+            ans += (csize - 1);
+        }
+        return ans;
+    }
+}
