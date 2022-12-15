a555bb2f00d091c25881d7128ac0cef1911aeb55
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-12-15 15:17:55 +0530

    Time: 3 ms (98.48%), Space: 42.8 MB (92.17%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads){
+        int[] degree = new int[101];
+        for(int[] road:roads){
+            int u = road[0];
+            int v = road[1];
+            degree[u]++;
+            degree[v]++;
+        }   
+        boolean[][] vis = new boolean[n][n];
+        for(int[] road:roads){
+            int u = road[0];
+            int v = road[1];
+            
+            vis[u][v] = true;
+            vis[v][u] = true;
+        }
+        int max = 0;
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                if(vis[i][j] == true || vis[j][i] == true){
+                    max = Math.max(max,degree[i] + degree[j]-1);
+                }else{
+                    max = Math.max(max,degree[i] + degree[j]);
+                }
+            }
+        }
+        return max;
+    }
+}
