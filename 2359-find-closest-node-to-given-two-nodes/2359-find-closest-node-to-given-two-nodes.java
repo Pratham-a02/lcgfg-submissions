b520558d0ba86378eb56163effb58ce9ddff4442
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-12-19 21:27:22 +0530

    Time: 45 ms (58.27%), Space: 116 MB (46.24%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) {
+        int n = edges.length;
+
+        int[] dist1 = new int[n];
+        Arrays.fill(dist1,Integer.MAX_VALUE);
+        dist1[node1]=0;
+
+        int[] dist2 = new int[n];
+        Arrays.fill(dist2,Integer.MAX_VALUE);
+        dist2[node2]=0;
+
+        boolean[] visited = new boolean[n];
+    
+        dfs(dist1,node1,visited,edges,0);
+
+        dfs(dist2,node2,visited,edges,0);
+
+        int min=Integer.MAX_VALUE;int ans=-1;
+        for(int i=0;i<n;i++){
+            if(dist1[i]!=Integer.MAX_VALUE && dist2[i]!=Integer.MAX_VALUE){
+                int max = Math.max(dist1[i],dist2[i]);
+                if(max<min){
+                    min = max;
+                    ans = i;
+                }
+            }
+        }
+        return ans;
+    }
+    public void dfs(int[] dist,int src,boolean[] visited,int[] edges,int d){
+
+        if(edges[src]==-1)
+            return;
+
+        visited[src]=true;
+
+        int dst=edges[src];
+        if(!visited[dst]){
+            dist[dst]=d+1;
+            dfs(dist,dst,visited,edges,d+1);
+        }
+
+        visited[src]=false;
+    }
+}
