a4a34a76ffd1de9786bba17e416e6b4a85699ba7
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2022-12-29 23:34:29 +0530

    Time: 1 ms (81.27%), Space: 39.2 MB (79.68%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumSwap(int num) {
+        char[] arr = (num+"").toCharArray();
+        int[] right = new int[arr.length];
+        right[arr.length-1] = -1;
+        
+        for(int i = arr.length-2;i>=0;i--){
+            if(i == arr.length-2){
+                right[arr.length-2] = arr.length -1;
+            }
+            else{
+                if(arr[i+1]>arr[right[i+1]]){
+                    right[i] = i+1;
+                }
+                else{
+                    right[i] = right[i+1];
+                }
+            }
+        }
+        
+        for(int i = 0;i<right.length-1;i++){
+            if(arr[i]<arr[right[i]]){
+                char temp = arr[i];
+                arr[i] = arr[right[i]];
+                arr[right[i]] = temp;
+                
+                break;
+            }
+        }
+        
+        return Integer.parseInt(new String(arr));
+    }
+}
