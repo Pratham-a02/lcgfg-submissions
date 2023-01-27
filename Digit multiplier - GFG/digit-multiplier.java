41e046e7476e762c4097f78ae8653089715ae8c1
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-01-27 22:07:13 +0530

    Added solution - LeetHub

---
+++ Digit multiplier - GFG/digit-multiplier.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.getSmallest(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String getSmallest(Long N){
+        StringBuilder sb = new StringBuilder();
+        if(N<=10){
+            return N+" ";
+        }
+        
+        for(int div = 9;div>1;div--){
+            while(N%div == 0){
+                sb.append(div);
+                N = N/div;
+            }
+        }
+        if(N>=10){
+            return "-1";
+        }
+        return sb.reverse().toString();
+    }
+}
