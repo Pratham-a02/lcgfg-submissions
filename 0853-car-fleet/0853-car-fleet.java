b394e16d820af6f2cd2bb90b0fbbe7365294ed8e
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-01-27 11:10:49 +0530

    Time: 172 ms (27.86%), Space: 84.1 MB (21.87%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,25 +1,29 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        TreeMap<Integer,Integer> hm = new TreeMap<>(Collections.reverseOrder());
+        int[][] dp = new int[position.length][2];
+        boolean[] vis = new boolean[position.length];
         for(int i = 0;i<position.length;i++){
-            hm.put(position[i],speed[i]);
+            dp[i][0] = position[i];
+            dp[i][1] = speed[i];
         }
         
-        int groups = 0;
-        double time = 0;
+        Arrays.sort(dp,(a,b)->{
+           return a[0] - b[0]; 
+        });
         
-        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
-            int pos = entry.getKey();
-            int sp = entry.getValue();
-            
+        int ans = 0;
+        double time = 0;
+        for(int i = dp.length-1;i>=0;i--){
+            int pos = dp[i][0];
             int dist = target - pos;
-            double ctime = dist*1.0/sp;
+            
+            double ctime = dist*1.0/dp[i][1];
             
             if(ctime>time){
                 time = ctime;
-                groups++;
+                ans++;
             }
         }
-        return groups;
+        return ans;
     }
 }
