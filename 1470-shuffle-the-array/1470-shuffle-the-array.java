e1a2eed562a05de4061dd3cfaef1600f8e109928
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-02-06 08:55:23 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (40.68%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        List<Integer> al = new ArrayList<>();
+        // List<Integer> al = new ArrayList<>();
+        int[] ans = new int[2*n];
         int i = 0;
         int j = n;
+        int x= 0;
         while(i<n && j<nums.length){
-            al.add(nums[i]);
-            al.add(nums[j]);
+            ans[x++] = nums[i];
+            ans[x++] = nums[j];
             i++;
             j++;
         }
-        int[] ans = new int[al.size()];
-        for(int k = 0;k<ans.length;k++){
-            ans[k] = al.get(k);
-        }
+        
+        // for(int k = 0;k<ans.length;k++){
+        //     ans[k] = al.get(k);
+        // }
         return ans;
     }
 }
