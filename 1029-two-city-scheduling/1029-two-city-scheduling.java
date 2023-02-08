47a0f9584c0acefa2c627b207aae2d0793c26ae1
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-02-08 23:12:29 +0530

    Time: 2 ms (74.19%), Space: 40.2 MB (91.37%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,53 +1,21 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int n = costs.length;
-        HashMap<int[],Integer> hm = new HashMap<>();
-        
-        for(int[]cost:costs){
-            int a = cost[0];
-            int b = cost[1];
-            
-            int diff = a-b;
-            hm.put(cost,diff);
-        }
-        
-        LinkedHashMap<int[],Integer> sortedMap = new LinkedHashMap<>();
-        List<Integer> list = new ArrayList<>();
-        
-        for (Map.Entry<int[],Integer> entry : hm.entrySet()) {
-            list.add(entry.getValue());
-        }
-        
-        // Collections.sort(list, new Comparator<Integer>(){
-        //     public int compare(int str, int str1) {
-        //         return str-str1;
-        //     }
-        // });
-        
-        Collections.sort(list, (d1, d2) ->{
-            return d1-d2;
+        Arrays.sort(costs,(a,b)->{
+            return (a[0]-a[1]) - (b[0]-b[1]);
         });
-        
-        for (int str : list) {
-            for (Map.Entry<int[], Integer> entry : hm.entrySet()) {
-                if (entry.getValue().equals(str)) {
-                    sortedMap.put(entry.getKey(), str);
-                }
-            }
-        }
         
         int ans = 0;
         int count = 0;
-        for(int[] arr:sortedMap.keySet()){
+        for(int[] arr:costs){
             if(count<n/2){
-                ans+=arr[0];
+                ans += arr[0];
             }
             else{
-                ans += arr[1];
+                ans+=arr[1];
             }
             count++;
         }
-        
         return ans;
     }
 }
