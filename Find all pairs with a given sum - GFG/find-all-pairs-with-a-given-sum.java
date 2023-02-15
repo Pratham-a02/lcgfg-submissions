3f8e4d12915a37ff2412e9044acae224cad02270
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-02-15 18:57:18 +0530

    Added solution - LeetHub

---
+++ Find all pairs with a given sum - GFG/find-all-pairs-with-a-given-sum.java
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long N = Long.parseLong(stt.nextToken());
+            long M = Long.parseLong(stt.nextToken());
+            long X = Long.parseLong(stt.nextToken());
+            long A[] = new long[(int)(N)];
+            long B[] = new long[(int)(M)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < N; i++) {
+                A[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < M; i++) {
+                B[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            pair [] answer = obj.allPairs(A, B, N, M, X);
+            int sz = answer.length;
+            
+            if(sz==0)
+            System.out.println(-1);
+            else{
+                StringBuilder output = new StringBuilder();
+                for(int i=0;i<sz;i++){
+                    if(i<sz-1)
+                    output.append(answer[i].first +" "+ answer[i].second + ", ");
+                    else
+                    output.append(answer[i].first +" "+ answer[i].second);
+                    
+                }
+                System.out.println(output);
+            }
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class pair  {  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+*/
+
+class Solution {
+    public pair[] allPairs( long A[], long B[], long N, long M, long X) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        ArrayList<pair> al = new ArrayList<>();
+        HashMap<Long,Long> hm = new HashMap<>();
+        // HashMap<Long,Long> hm2 = new HashMap<>();
+        
+        // for(int i = 0;i<A.length;i++){
+        //     hm1.put(A[i],1L);
+        // }
+        
+        for(int i = 0;i<B.length;i++){
+            hm.put(B[i],1L);
+        }
+        
+        for(int i = 0;i<A.length;i++){
+            if(hm.containsKey(X-A[i])){
+                pair put = new pair(A[i],X-A[i]);
+                al.add(put);
+            }
+        }
+        
+        pair[] ans = new pair[al.size()];
+        
+        for(int i = 0;i<ans.length;i++){
+            ans[i] = al.get(i);
+        }
+        
+        return ans;
+    }
+}
