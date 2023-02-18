4ef8f6c4ec31dcc0f3f4aaf64c1683a05e81e7cf
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-02-18 17:51:54 +0530

    Time: 49 ms (20.43%), Space: 42.8 MB (71.89%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int partitionString(String s) {
+        Stack<Character> st = new Stack<>();
+        int count = 0;
+        for(int i = 0;i<s.length();i++){
+            if(st.contains(s.charAt(i))){
+                st.clear();
+                count++;
+                st.push(s.charAt(i));
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        if(st.size()>0){
+            count++;
+        }
+        return count;
+    }
+}
