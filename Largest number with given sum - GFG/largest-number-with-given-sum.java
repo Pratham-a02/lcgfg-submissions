14460bd0edec1e28ec339a980e45fdb706ca5598
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-02-24 17:08:16 +0530

    Added solution - LeetHub

---
+++ Largest number with given sum - GFG/largest-number-with-given-sum.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		
+	    while(t-- >0)
+	    {
+	        String inputLine[] = br.readLine().trim().split(" ");
+	        
+	        //taking n and sum
+            int n = Integer.parseInt(inputLine[0]);
+            int sum = Integer.parseInt(inputLine[1]);
+            
+            //calling largestNumber() function
+            System.out.println(new Solution().largestNumber(n, sum));
+	    }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to return the largest possible number of n digits
+    //with sum equal to given sum.
+    public static String largestNumber(int n, int sum){
+        if(sum > 9*n){
+            return "-1";
+        }
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0;i<n;i++){
+            if(sum>=9){
+                sum -= 9;
+                ans.append(9);
+            }
+            else{
+                ans.append(sum);
+                sum = 0;
+            }
+        }
+        return ans.toString();
+    }
+}
+
+
