1f3e395fac9e949d141054c1b7e5a318c9f27707
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-02 15:47:25 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.java
+++ Common elements - GFG/common-elements.java
@@ -56,44 +56,31 @@ public class GFG
 
 //User function Template for Java
 
-class Solution
-{
-    public ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) {
+class Solution{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3){
         ArrayList<Integer> ans = new ArrayList<>();
-        LinkedHashMap<Integer,Integer> hm1 = new LinkedHashMap<>();
-        LinkedHashMap<Integer,Integer> hm2 = new LinkedHashMap<>();
-        LinkedHashMap<Integer,Integer> hm3 = new LinkedHashMap<>();
         int i = 0;
         int j = 0;
         int k = 0;
         while(i<n1 && j<n2 && k<n3){
-            hm1.put(A[i],hm1.getOrDefault(A[i],0)+1);
-            hm2.put(B[j],hm2.getOrDefault(B[j],0)+1);
-            hm3.put(C[k],hm3.getOrDefault(C[k],0)+1);
-            
-            i++;
-            j++;
-            k++;
-        }
-        while(i<n1){
-            hm1.put(A[i],hm1.getOrDefault(A[i],0)+1);
-            i++;
-        } 
-        while(j<n2){
-            hm2.put(B[j],hm2.getOrDefault(B[j],0)+1);
-            j++;
-        }
-        while(k<n3){
-            hm3.put(C[k],hm3.getOrDefault(C[k],0)+1);
-            k++;
-        }
-        
-        for(int key:hm1.keySet()){
-            if(hm2.containsKey(key) && hm3.containsKey(key)){
-                ans.add(key);
+            if(A[i] == B[j] && B[j] == C[k]){
+                if(!ans.contains(A[i])){
+                    ans.add(A[i]);
+                }
+                i++;
+                j++;
+                k++;
+            }
+            else if(A[i]<B[j]){
+                i++;
+            }
+            else if(B[j]<C[k]){
+                j++;
+            }
+            else{
+                k++;
             }
         }
-        // Collections.sort(ans);
         return ans;
     }
 }
