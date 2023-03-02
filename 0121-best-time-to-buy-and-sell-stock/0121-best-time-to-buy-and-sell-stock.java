26dcf5448224ba933bab72502910f0cb645630d7
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-02 14:47:30 +0530

    Time: 148 ms (5.79%), Space: 81.2 MB (10.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,34 +1,33 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int[][][] dp = new int[prices.length+1][2][2];
-        for(int[][] arr:dp){
-            for(int[] arr1:arr)
-                Arrays.fill(arr1,-1);
+        for(int[][] arr1:dp){
+            for(int[] arr2:arr1){
+                Arrays.fill(arr2,-1);
+            }
         }
         return getMax(0,1,1,prices,dp);
     }
     
-    public int getMax(int idx,int canBuy,int count,int[] prices,int[][][] dp){
-        if(idx>=prices.length){
-            return 0;
-        }
-        if(count == 0){
+    public int getMax(int currIdx,int trans,int canBuy,int[] prices,int[][][] dp){
+        if(currIdx>=prices.length || trans == 0){
             return 0;
         }
         
-        if(dp[idx][count][canBuy] != -1){
-            return dp[idx][count][canBuy];
+        if(dp[currIdx][trans][canBuy] != -1){
+            return dp[currIdx][trans][canBuy];
         }
         
         if(canBuy == 1){
-            int idle = getMax(idx+1,1,count,prices,dp);
-            int buy = -prices[idx] + getMax(idx+1,0,count,prices,dp);
-            return dp[idx][count][canBuy] = Math.max(idle,buy);
+            int idle = getMax(currIdx+1,trans,1,prices,dp);
+            int buy = -prices[currIdx] + getMax(currIdx+1,trans,0,prices,dp);
+            
+            return dp[currIdx][trans][canBuy] = Math.max(idle,buy);
         }
         else{
-            int idle  = getMax(idx+1,0,count,prices,dp);
-            int sell = prices[idx] + getMax(idx+1,1,count-1,prices,dp);
-            return dp[idx][count][canBuy] = Math.max(idle,sell);
+            int idle = getMax(currIdx+1,trans,0,prices,dp);
+            int sell = prices[currIdx] + getMax(currIdx+1,trans-1,1,prices,dp);
+            return dp[currIdx][trans][canBuy] = Math.max(idle,sell);
         }
     }
 }
