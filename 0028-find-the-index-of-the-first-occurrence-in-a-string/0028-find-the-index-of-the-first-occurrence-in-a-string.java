859b2be80c733f87028026be8982baeee8ae22a2
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-03 12:01:37 +0530

    Time: 9 ms (5.37%), Space: 40.3 MB (58.02%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int strStr(String txt, String pat) {
+       int[] lps = getLPS(pat);
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        
+        while(i<txt.length()){
+            if(txt.charAt(i) == pat.charAt(j)){
+                i++;
+                j++;
+                
+                if(j == pat.length()){
+                    return (i-j);
+                    // j = lps[j-1];
+                }
+            }
+            else if(j == 0){
+                i++;
+            }
+            else{
+                j = lps[j-1];
+            }
+        }
+        return -1;
+    }
+    
+    public static int[] getLPS(String pat){
+        int len = 0;
+        int i = 1;
+        int[] lps = new int[pat.length()];
+        // lps[0] = 0;
+        while(i<pat.length()){
+            if(pat.charAt(i) == pat.charAt(len)){
+                lps[i] = len+1;
+                len++;
+                i++;
+            }
+            else if(len == 0){
+                lps[i] = 0;
+                len = 0;
+                i++;
+            }
+            else{
+                len = lps[len-1];
+            }
+        }
+        for(int val:lps){
+            System.out.print(val+" ");
+        }
+        return lps;
+    }
+}
