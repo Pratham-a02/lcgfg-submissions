b91834cfc4d208b2f57b8832d9b629483a89985c
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-08 12:05:11 +0530

    Added solution - LeetHub

---
+++ Split the binary string into substrings with equal number of 0s and 1s - GFG/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    public static int maxSubStr(String str){
+       int count0 = 0;
+       int count1 = 0;
+       int count = 0;
+       
+       for(int i = 0;i<str.length();i++){
+           if(str.charAt(i) == '0'){
+               count0++;
+           }
+           if(str.charAt(i) == '1'){
+               count1++;
+           }
+           
+           if(count0 == count1){
+               count++;
+               count0 = 0;
+               count1 = 0;
+           }
+       }
+       if(count==0||count0!=0||count1!=0){
+            return -1;
+        }
+        else{
+            return count;
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	    var sc = new FastReader();
+	    int test = sc.nextInt();
+	    while(test-->0){
+    		String s = sc.nextString();
+    		System.out.println(Solution.maxSubStr(s));
+	    }
+	}
+    
+    public static class FastReader
+	{
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader()
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
+
+		private void read() throws IOException
+		{
+			st = new StringTokenizer(br.readLine());
+		}
+
+		public String nextString() throws IOException
+		{
+			while(!st.hasMoreTokens())
+				read();
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException
+		{
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() throws IOException
+		{
+			return Long.parseLong(nextString());
+		}
+	}
+}
+// } Driver Code Ends
