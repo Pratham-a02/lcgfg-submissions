bf5744655543a2822a71053b729602c06f2062c2
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-10 08:43:21 +0530

    Time: 14 ms (23.36%), Space: 44.9 MB (16.06%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode head = null;
+    Random rand = null;
+    public Solution(ListNode head) {
+        this.head = head;
+        rand = new Random();
+    }
+    
+    public int getRandom() {
+        int res = -1;
+        ListNode t = head;
+        for(int i = 1;t!=null;i++){
+            if(rand.nextInt(i) == i-1){
+                res = t.val;
+            }
+            t = t.next;
+        }
+        return res;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
