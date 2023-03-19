9db5627234bd1f1f984e6d4fee317b7c5189f216
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-19 18:06:45 +0530

    Time: 8 ms (33.57%), Space: 42.7 MB (52.23%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -3,7 +3,7 @@ class Solution {
     int n;
     public List<String> restoreIpAddresses(String s) {
         n = s.length();
-        if(s.length()<4 || s.length()>12){
+        if(s.length()>12){
             return new ArrayList<>();
         }
         ans = new ArrayList<>();
@@ -12,7 +12,7 @@ class Solution {
     }
     
     public void solve(int idx,int parts,String curr,String s){
-        if(idx == n && parts == 4){
+        if(idx == s.length() && parts == 4){
             curr = curr.substring(0,curr.length()-1);
             ans.add(curr);
         }
