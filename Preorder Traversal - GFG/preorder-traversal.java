160c4f786fa0f8b54d33679a0b816d1034fbf18d
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-20 10:55:00 +0530

    Added solution - LeetHub

---
+++ Preorder Traversal - GFG/preorder-traversal.java
@@ -0,0 +1,157 @@
+//{ Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                ArrayList<Integer> res = BinaryTree.preorder(root);
+                for (Integer num : res)
+                    System.out.print(num + " ");
+                System.out.println();
+                t--;
+            
+        }
+    }
+   
+}
+
+
+// } Driver Code Ends
+// class Node{
+//     int data;
+//     Node left,right;
+//     Node(int d){
+//         data=d;
+//         left=right=null;
+//     }
+// }
+class BinaryTree{
+    static class Pair{
+        Node node;
+        int state;
+        Pair(Node node,int state){
+            this.node = node;
+            this.state = state;
+        }
+    }
+    
+    public static ArrayList<Integer> preorder(Node root){
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Pair> st = new Stack<>();
+        Pair p = new Pair(root,1);
+        st.push(p);
+        
+        while(st.size()>0){
+            Pair top = st.peek();
+            if(top.state == 1){
+                ans.add(top.node.data);
+                top.state++;
+                
+                if(top.node.left != null){
+                    Pair lp = new Pair(top.node.left,1);
+                    st.push(lp);
+                }
+            }
+            else if(top.state == 2){
+                top.state++;
+                if(top.node.right != null){
+                    Pair rp = new Pair(top.node.right,1);
+                    st.push(rp);
+                }
+            }
+            else{
+                st.pop();
+            }
+        }
+        
+        return ans;
+    }
+}
