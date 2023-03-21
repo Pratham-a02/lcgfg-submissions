34cd2b1d7cd99fb92b59833887e40da0153369cb
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-21 18:06:27 +0530

    Added solution - LeetHub

---
+++ Largest subtree sum in a tree - GFG/largest-subtree-sum-in-a-tree.java
@@ -0,0 +1,150 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node root = Node.inputTree(br);
+            
+            Solution obj = new Solution();
+            int res = obj.findLargestSubtreeSum(root);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+/*
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution{
+    static int max = Integer.MIN_VALUE;
+    public static int findLargestSubtreeSum(Node root){
+        max = Integer.MIN_VALUE;
+        dfs(root);
+        return max;
+    }
+    
+    public static int dfs(Node root){
+        if(root == null){
+            return 0;
+        }
+        
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        
+        max = Math.max(max,left+right+root.data);
+        return left+right+root.data;
+    }
+}
+        
