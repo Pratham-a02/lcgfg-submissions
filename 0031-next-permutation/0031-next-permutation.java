3340ab945c6bd28223be1f4cf75e206c265a96c8
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-03-25 11:18:51 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (24.16%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,38 +1,31 @@
-class Solution{
-    public static void nextPermutation(int[] arr){
-        int N = arr.length;
-        int i = N-2;
-        while(i>= 0 && arr[i]>=arr[i+1]){
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
-        
         if(i>=0){
-            int j = N-1;
-            while(j>= 0 && arr[j]<=arr[i]){
+            int j = nums.length-1;
+            while(j>=0 && nums[j]<=nums[i]){
                 j--;
             }
-            
-            swap(i,j,arr);
+            swap(i,j,nums);
         }
-        reverse(i+1,N-1,arr);
-        // List<Integer> ans = new ArrayList<>();
-        // for(int val:arr){
-        //     ans.add(val);
-        // }
-        // return ans;
+        
+        reverse(i+1,nums.length-1,nums);
     }
     
-    public static void swap(int i,int j,int[] arr){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
-    public static void reverse(int i,int j,int[] arr){
+    public void reverse(int i,int j,int[] nums){
         while(i<j){
-            swap(i,j,arr);
+            swap(i,j,nums);
             i++;
             j--;
         }
+    }
+    
+    public void swap(int i,int j,int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
