38bbfccc35c4f5ca993cf698b2f866dd8fa902ce
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-13 16:47:36 +0530

    Time: 1 ms (61.24%), Space: 42.4 MB (34.26%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[cost.length+1];
         return minCost(cost.length,cost,dp);
     }
-    
+    /*
     public int minCost(int n,int[] cost,int[] dp){
         if(n<=1){
             return dp[n] = cost[n];
@@ -14,5 +14,18 @@ class Solution {
         }
         
         return dp[n] = Math.min(minCost(n-1,cost,dp),minCost(n-2,cost,dp)) + (n == cost.length?0:cost[n]);
+    }
+    */
+    
+    public int minCost(int N,int[] cost,int[] dp){
+        for(int n = 0;n<=N;n++){
+            if(n<=1){
+                dp[n] = cost[n];
+                continue;
+            }
+        
+            dp[n] = Math.min(dp[n-1],dp[n-2]) + (n == cost.length?0:cost[n]);
+        }
+        return dp[N];
     }
 }
