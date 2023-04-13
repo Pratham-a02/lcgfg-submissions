096ebbbc713acdd4ceff0fd9ac1b09c388879b08
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-13 15:04:33 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (68.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        return totalWays(0,n,dp);
+        return climbStairs_M(n,dp);
     }
     
-    public int totalWays(int curr,int n,int[] dp){
-        if(curr == n){
-            return 1;
+    public int climbStairs_M(int n,int[] dp){
+        if(n<=1){
+            return dp[n] = 1;
         }
-        if(curr>n){
-            return 0;
+        if(dp[n] != 0){
+            return dp[n];
         }
-        if(dp[curr] != 0){
-            return dp[curr];
-        }
-        int count = 0;
-        
-        count += totalWays(curr+1,n,dp);
-        count += totalWays(curr+2,n,dp);
-        
-        return dp[curr] = count;
+        return dp[n] = climbStairs_M(n-1,dp) + climbStairs_M(n-2,dp);
     }
 }
