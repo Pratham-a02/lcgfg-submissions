98e6e4ed31d1b401394fc5db87feaf42f3be917a
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-14 09:50:05 +0530

    Time: 52 ms (74.87%), Space: 52.1 MB (35.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,30 +1,50 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()+1][s.length()+1];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
-        }
+        int[][] dp = new int[s.length()][s.length()];
+        // for(int[] arr:dp){
+        //     Arrays.fill(arr,-1);
+        // }
         
         return lps(s,0,s.length()-1,dp);
     }
     
-    public int lps(String s,int i,int j,int[][] dp){
-        if(i>j){
-            return 0;
-        }
-        if(i == j){
-            return 1;
-        }
+//     public int lps(String s,int i,int j,int[][] dp){
+//         if(i>j){
+//             return 0;
+//         }
+//         if(i == j){
+//             return 1;
+//         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
         
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = lps(s,i+1,j-1,dp) + 2;
+//         if(s.charAt(i) == s.charAt(j)){
+//             return dp[i][j] = lps(s,i+1,j-1,dp) + 2;
+//         }
+//         else{
+//             return dp[i][j] = Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
+//         }
+//     }
+    
+    public int lps(String s,int I,int J,int[][] dp){
+        int n = s.length();
+        for(int gap = 0;gap<n;gap++){
+            for(int i = 0,j = gap;j<n;i++,j++){
+                if(i>=j){
+                    dp[i][j] = (i==j)?1:0;
+                    continue;
+                }
+
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i+1][j] , dp[i][j-1]);
+                }
+            }
         }
-        else{
-            return dp[i][j] = Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
-        }
+        return dp[I][J];
     }
 }
