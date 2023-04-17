4050c609a7c759eb21b495dfb70eadc6a4aefe13
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-17 11:00:15 +0530

    Time: 74 ms (33.87%), Space: 42.6 MB (28.27%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,11 +1,11 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
-        int len = 0;
-        for(int i = 0;i<nums.length;i++){
-            len = Math.max(len,LIS_R(i,nums,dp));
-        }
-        return len;
+        // int len = 0;
+        // for(int i = 0;i<nums.length;i++){
+        //     len = Math.max(len,LIS_R(i,nums,dp));
+        // }
+        return LIS_Tab(nums,dp);
     }
     
     public int LIS_R(int i,int[] nums,int[] dp){
@@ -21,5 +21,19 @@ class Solution {
         }
         
         return dp[i] = longestLen;
+    }
+    
+    public int LIS_Tab(int[] nums,int[] dp){
+        int len = 0;
+        for(int i = 0;i<nums.length;i++){
+            dp[i] = 1;
+            for(int j = 0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+            len = Math.max(len,dp[i]);
+        }
+        return len;
     }
 }
