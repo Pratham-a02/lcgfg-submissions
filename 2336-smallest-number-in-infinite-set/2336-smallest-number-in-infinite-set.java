a1eda9707da1c59b969108edf56bd4fd51c69b6a
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-25 07:39:54 +0530

    Time: 35 ms (33.14%), Space: 43.2 MB (53.82%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.java
@@ -1,29 +1,52 @@
-class SmallestInfiniteSet {
+/*class SmallestInfiniteSet{
     PriorityQueue<Integer> pq;
     HashSet<Integer> hs;
-    public SmallestInfiniteSet() {
+    public SmallestInfiniteSet(){
         pq = new PriorityQueue<>();
         hs = new HashSet<>();
-        
+        // numbers are from 1 to 1000, so add all the numbers.
         for(int i = 1;i<=1000;i++){
             pq.add(i);
             hs.add(i);
         }
     }
     
-    public int popSmallest() {
+    public int popSmallest(){
+        // for getting the smallest element, pq is always used;
         int x = pq.remove();
         hs.remove(x);
         return x;
     }
     
-    public void addBack(int num) {
+    public void addBack(int num){
+        // add in priorityqueue only when it is not presented in set.c
         if(!hs.contains(num)){
             pq.add(num);
             hs.add(num);
         }
     }
 }
+*/
+class SmallestInfiniteSet{
+    TreeSet<Integer> hs;
+    public SmallestInfiniteSet(){
+        hs = new TreeSet<>();
+        
+        for(int i = 1;i<=1000;i++){
+            hs.add(i);
+        }
+    }
+    
+    public int popSmallest(){
+        int x = hs.pollFirst();
+        return x;
+    }
+    
+    public void addBack(int num){
+        hs.add(num);
+    }
+}
+
 
 /**
  * Your SmallestInfiniteSet object will be instantiated and called as such:
