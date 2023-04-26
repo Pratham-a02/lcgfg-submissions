b55521b8644f4e357a01372b0bbd8b84ebff2b76
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-26 09:17:09 +0530

    Time: 13 ms (25.73%), Space: 43.4 MB (33.99%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -1,28 +1,24 @@
-class Solution {
+class Pair{
+    int x;
+    int y;
+    int level;
     
-    class Pair{
-        int i;
-        int j;
-        int level;
-        
-        Pair(int i,int j,int level){
-            this.i = i;
-            this.j = j;
-            this.level = level;
-        }
+    Pair(int x,int y,int level){
+        this.x = x;
+        this.y = y;
+        this.level = level;
     }
-    
-    public int shortestBridge(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] vis = new boolean[m][n];
+}
+
+class Solution{
+    public int shortestBridge(int[][] grid){
         ArrayDeque<Pair> qu = new ArrayDeque<>();
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
         boolean found = false;
-        
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j] == 1){
-                    helper(i,j,grid,qu,vis);
+                    dfs(i,j,qu,grid,vis);
                     found = true;
                     break;
                 }
@@ -32,49 +28,54 @@ class Solution {
             }
         }
         
-        boolean[][] visited = new boolean[m][n];
+        boolean[][] vis2 = new boolean[grid.length][grid[0].length];
         while(qu.size()>0){
             Pair rem = qu.remove();
             
-            if(visited[rem.i][rem.j] == true){
+            if(vis2[rem.x][rem.y] == true){
                 continue;
             }
-            visited[rem.i][rem.j] = true;
             
-            if(grid[rem.i][rem.j] == 1){
-                return rem.level-1;
+            vis2[rem.x][rem.y] = true;
+            if(grid[rem.x][rem.y] == 1){
+                return rem.level - 1;
             }
             
-            if(rem.i-1>= 0 && grid[rem.i-1][rem.j] != 2 && visited[rem.i-1][rem.j] != true){
-                qu.add(new Pair(rem.i-1,rem.j,rem.level+1));
-            }
-            if(rem.i+1<m && grid[rem.i+1][rem.j] != 2 && visited[rem.i+1][rem.j] != true){
-                qu.add(new Pair(rem.i+1,rem.j,rem.level+1));
-            }
-            if(rem.j-1>= 0 && grid[rem.i][rem.j-1] != 2 && visited[rem.i][rem.j-1] != true){
-                qu.add(new Pair(rem.i,rem.j-1,rem.level+1));
-            }
-            if(rem.j+1<n && grid[rem.i][rem.j+1] != 2 && visited[rem.i][rem.j+1] != true){
-                qu.add(new Pair(rem.i,rem.j+1,rem.level+1));
-            }
+            addNbr(rem.x-1,rem.y,rem.level,qu,vis2,grid);
+            addNbr(rem.x,rem.y+1,rem.level,qu,vis2,grid);
+            addNbr(rem.x+1,rem.y,rem.level,qu,vis2,grid);
+            addNbr(rem.x,rem.y-1,rem.level,qu,vis2,grid);
         }
         return -1;
     }
     
-    public void helper(int currRow,int currCol,int[][] grid,ArrayDeque<Pair> qu,boolean[][] vis){
-        if(currRow<0 || currCol<0 || currRow>=grid.length || currCol>=grid[0].length || grid[currRow][currCol] == 0 || vis[currRow][currCol] == true){
+    public void addNbr(int cr,int cc,int level,ArrayDeque<Pair> qu,boolean[][] vis2,int[][] grid){
+        if(cr<0 || cc<0 || cr>=grid.length || cc>=grid.length){
+            return;
+        }
+        else if(grid[cr][cc] == 2){
+            return;
+        }
+        else if(vis2[cr][cc] == true){
             return;
         }
         
-        vis[currRow][currCol] = true;
-        qu.add(new Pair(currRow,currCol,0));
+        qu.add(new Pair(cr,cc,level+1));
+    }
+    
+    public void dfs(int cr,int cc,ArrayDeque<Pair> qu,int[][] grid,boolean[][] vis){
+        if(cr<0 || cc<0 || cr>=grid.length || cc >= grid.length || grid[cr][cc] == 0 ||vis[cr][cc] == true){
+            return;
+        }
         
-        helper(currRow-1,currCol,grid,qu,vis);
-        helper(currRow,currCol-1,grid,qu,vis);
-        helper(currRow+1,currCol,grid,qu,vis);
-        helper(currRow,currCol+1,grid,qu,vis);
+        vis[cr][cc] = true;
+        qu.add(new Pair(cr,cc,0));
         
-        grid[currRow][currCol] = 2;
+        dfs(cr-1,cc,qu,grid,vis);
+        dfs(cr,cc+1,qu,grid,vis);
+        dfs(cr+1,cc,qu,grid,vis);
+        dfs(cr,cc-1,qu,grid,vis);
+        grid[cr][cc] = 2;
         return;
     }
 }
