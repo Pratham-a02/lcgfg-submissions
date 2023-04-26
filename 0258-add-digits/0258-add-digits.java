6477b6bc9bff9cbd9728840a5b4d89ceb56fda66
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-26 07:55:24 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (56.73%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,29 +1,31 @@
 class Solution {
-    public int addDigits(int num) {
-        String str = Integer.toString(num);
+    public int addDigits(int num){
+        if(num == 0) return 0;
+        return (num%9) == 0?9:num%9;
+//         String str = Integer.toString(num);
         
-        boolean flag = false;
-        String s = "";
-        while(s.length() != 1){
-            int sum = 0;
+//         boolean flag = false;
+//         String s = "";
+//         while(s.length() != 1){
+//             int sum = 0;
             
-            if(flag == false){
-                char[] arr = str.toCharArray();
-                for(int i = 0;i<arr.length;i++){
-                    sum += (arr[i]-'0');
-                }
-                s = String.valueOf(sum);
-                flag = true;
-            }
-            else{
-                char[] arr = s.toCharArray();
-                for(int i = 0;i<arr.length;i++){
-                    sum += (arr[i]-'0');
-                }
-                s = String.valueOf(sum);
-            }
-        }
+//             if(flag == false){
+//                 char[] arr = str.toCharArray();
+//                 for(int i = 0;i<arr.length;i++){
+//                     sum += (arr[i]-'0');
+//                 }
+//                 s = String.valueOf(sum);
+//                 flag = true;
+//             }
+//             else{
+//                 char[] arr = s.toCharArray();
+//                 for(int i = 0;i<arr.length;i++){
+//                     sum += (arr[i]-'0');
+//                 }
+//                 s = String.valueOf(sum);
+//             }
+//         }
         
-        return Integer.parseInt(s);
+//         return Integer.parseInt(s);
     }
 }
