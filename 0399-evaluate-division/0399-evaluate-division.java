d16338638339c85cb5a315b8a01370886c77bddd
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-27 18:51:38 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (47.25%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,55 +1,11 @@
-class Solution {
+class Solution{
     HashMap<String,String> parent;
-    HashMap<String,Double> multi;
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        parent = new HashMap<>();
-        multi = new HashMap<>();
-        for(List<String>eqn:equations){
-            addToDSU(eqn.get(0));
-            addToDSU(eqn.get(1));
-        }
-        int i = 0;
-        for(List<String> eqn:equations){
-            String l0 = find(eqn.get(0));
-            String l1 = find(eqn.get(1));
-            
-            double m0 = multi.get(eqn.get(0));
-            double m1 = multi.get(eqn.get(1));
-            
-            multi.put(l0,(m1*values[i])/m0);
-            parent.put(l0,l1);
-            i++;
-        }
-        double[] res = new double[queries.size()];
-        i = 0;
-        for(List<String> query:queries){
-            if(parent.containsKey(query.get(0)) == false || parent.containsKey(query.get(1)) == false){
-                res[i] = -1;
-                i++;
-                continue;
-            }
-            
-            String l0 = find(query.get(0));
-            String l1 = find(query.get(1));
-            
-            if(l0.equals(l1) == false){
-                res[i] = -1;
-                i++;
-                continue;
-            }
-            
-            double m0 = multi.get(query.get(0));
-            double m1 = multi.get(query.get(1));
-            
-            res[i] = m0/m1;
-            i++;
-        }
-        return res;
-    }
-    
-    public void addToDSU(String x){
-        parent.put(x,x);
-        multi.put(x,1.0);
+    HashMap<String,Double> mult;
+    public void addDSU(String x){
+        
+            parent.put(x,x);
+            mult.put(x,1.0);
+        
     }
     
     public String find(String x){
@@ -62,9 +18,62 @@ class Solution {
             
             parent.put(x,finalparent);
             
-            multi.put(x,multi.get(currparent)*multi.get(x));
+            mult.put(x,mult.get(currparent)*mult.get(x));
                     return finalparent;
 
         }
     }
+    
+    
+    
+    
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries){
+        parent = new HashMap<>();
+        mult = new HashMap<>();
+        
+        for(List<String> eqn:equations){
+            addDSU(eqn.get(0));
+            addDSU(eqn.get(1));
+        }
+        int i = 0;
+        for(List<String> eqn : equations){
+            String l0 = find(eqn.get(0));
+            String l1  = find(eqn.get(1));
+            
+            double m1 = mult.get(eqn.get(0));
+            double m2 = mult.get(eqn.get(1));
+
+            mult.put(l0,(m2*values[i])/m1);
+            parent.put(l0,l1);
+            i++;
+        }
+        i = 0;
+        double[] res = new double[queries.size()];
+        for(List<String> qu :queries){
+            if(parent.containsKey(qu.get(0)) == false || parent.containsKey(qu.get(1)) == false){
+                res[i] = -1;
+                i++;
+                continue;
+            }
+            
+            String l0 = find(qu.get(0));
+            String l1 = find(qu.get(1));
+            
+            if(l0.equals(l1) == false){
+                res[i] = -1;
+                i++;
+                continue;
+            }
+            
+            double m1 = mult.get(qu.get(0));
+            double m2 = mult.get(qu.get(1));
+            res[i] = m1/m2;
+            i++;
+        }
+        return res;
+    }
+    
+    
+    
+    
 }
