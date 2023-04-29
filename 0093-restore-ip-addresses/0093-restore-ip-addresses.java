6679dfcfcbe751688101d6c339a01ff25ef91e91
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-04-29 15:12:53 +0530

    Time: 12 ms (8.71%), Space: 43.2 MB (9.18%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,35 +1,28 @@
 class Solution {
-    List<String> ans;
-    int n;
     public List<String> restoreIpAddresses(String s) {
-        n = s.length();
-        if(s.length()>12){
-            return new ArrayList<>();
-        }
-        ans = new ArrayList<>();
-        solve(0,0,"",s);
+        List<String> ans = new ArrayList<>();
+        solve(0,0,"",s,ans);
         return ans;
     }
     
-    public void solve(int idx,int parts,String curr,String s){
+    public void solve(int idx,int parts,String curr,String s,List<String> ans){
         if(idx == s.length() && parts == 4){
-            curr = curr.substring(0,curr.length()-1);
-            ans.add(curr);
+            ans.add(curr.substring(0,curr.length()-1));
         }
         
-        if(idx+1<=s.length()){
-            solve(idx+1,parts+1,curr + s.substring(idx,idx+1) + ".",s);
+        if(idx + 1 <= s.length()){
+            solve(idx+1,parts+1,curr + s.substring(idx,idx+1) +  ".",s,ans);
         }
-        if(idx+2<=s.length() && isValid(s.substring(idx,idx+2))){
-            solve(idx+2,parts+1,curr + s.substring(idx,idx+2) + ".",s);
+        if(idx + 2 <= s.length() && isValid(s.substring(idx,idx+2))){
+            solve(idx+2,parts+1,curr + s.substring(idx,idx+2) +  ".",s,ans);
         }
-        if(idx+3<=s.length() && isValid(s.substring(idx,idx+3))){
-            solve(idx+3,parts+1,curr + s.substring(idx,idx+3) + ".",s);
+        if(idx + 3 <= s.length() && isValid(s.substring(idx,idx+3))){
+            solve(idx+3,parts+1,curr + s.substring(idx,idx+3) +  ".",s,ans);
         }
     }
     
     public boolean isValid(String s){
-        if(s.charAt(0) == '0' || Integer.valueOf(s)>255){
+        if(s.charAt(0) == '0' || Integer.parseInt(s) > 255){
             return false;
         }
         return true;
