7090048322c86cd629c43ebaa18c8299909fa071
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-01 17:06:02 +0530

    Time: 1 ms (99.92%), Space: 46.9 MB (15.32%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,24 +1,26 @@
-class Solution {
+class Solution{
     public List<Integer> majorityElement(int[] nums){
         List<Integer> ans = new ArrayList<>();
         int count1 = 0;
-        int can1 = 0;
         int count2 = 0;
+        
+        
+        int can1 = 0;
         int can2 = 0;
         
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] == can1){
+        for(int ele : nums){
+            if(ele == can1){
                 count1++;
             }
-            else if(nums[i] == can2){
+            else if(ele == can2){
                 count2++;
             }
             else if(count1 == 0){
-                can1 = nums[i];
+                can1 = ele;
                 count1 = 1;
             }
             else if(count2 == 0){
-                can2 = nums[i];
+                can2 = ele;
                 count2 = 1;
             }
             else{
@@ -29,6 +31,7 @@ class Solution {
         
         count1 = 0;
         count2 = 0;
+        
         for(int i = 0;i<nums.length;i++){
             if(nums[i] == can1){
                 count1++;
@@ -37,12 +40,10 @@ class Solution {
                 count2++;
             }
         }
-        
-        if(count1>(nums.length/3)){
+        if(count1 > nums.length/3){
             ans.add(can1);
         }
-        
-        if(count2>(nums.length/3)){
+        if(count2 > nums.length/3){
             ans.add(can2);
         }
         
