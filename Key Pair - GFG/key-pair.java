16469f12cc137e848de1ff51c171dbd179655138
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-03 18:51:58 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.java
+++ Key Pair - GFG/key-pair.java
@@ -31,22 +31,18 @@ public class Main {
 
 // } Driver Code Ends
 
-
-//User function Template for Java
-
-
-class Solution {
-    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
-       HashMap<Integer,Integer> hm = new HashMap<>();
-       for(int i = 0;i<n;i++){
-           int val = arr[i];
-           if(hm.containsKey(x - val)){
-               return true;
-           }
-           else{
-               hm.put(val,1);
-           }
-       }
-       return false;
+class Solution{
+    boolean hasArrayTwoCandidates(int arr[],int n,int x){
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i = 0;i<n;i++){
+            int val = arr[i];
+            
+            if(hm.containsKey(x-val)){
+                return true;
+            }
+            hm.put(val,hm.getOrDefault(val,0)+1);
+        }
+        return false;
     }
 }
