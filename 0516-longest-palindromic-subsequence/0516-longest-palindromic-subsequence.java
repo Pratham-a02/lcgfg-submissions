372bd0c72a7afcf51cfcd09033f2d990100d42af
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-04 14:29:50 +0530

    Time: 102 ms (11.54%), Space: 52.6 MB (9.15%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,50 +1,27 @@
-class Solution {
-    public int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-        // for(int[] arr:dp){
-        //     Arrays.fill(arr,-1);
-        // }
-        
-        return lps(s,0,s.length()-1,dp);
+class Solution{
+    public int longestPalindromeSubseq(String s){
+        int[][] dp = new int[1001][1001];
+        for(int[]d:dp){
+            Arrays.fill(d,-1);
+        }
+        return lps(0,s.length()-1,s,dp);
     }
     
-//     public int lps(String s,int i,int j,int[][] dp){
-//         if(i>j){
-//             return 0;
-//         }
-//         if(i == j){
-//             return 1;
-//         }
-        
-//         if(dp[i][j] != -1){
-//             return dp[i][j];
-//         }
-        
-//         if(s.charAt(i) == s.charAt(j)){
-//             return dp[i][j] = lps(s,i+1,j-1,dp) + 2;
-//         }
-//         else{
-//             return dp[i][j] = Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
-//         }
-//     }
-    
-    public int lps(String s,int I,int J,int[][] dp){
-        int n = s.length();
-        for(int gap = 0;gap<n;gap++){
-            for(int i = 0,j = gap;j<n;i++,j++){
-                if(i>=j){
-                    dp[i][j] = (i==j)?1:0;
-                    continue;
-                }
-
-                if(s.charAt(i) == s.charAt(j)){
-                    dp[i][j] = dp[i+1][j-1] + 2;
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i+1][j] , dp[i][j-1]);
-                }
-            }
+    public int lps(int i,int j,String s,int[][] dp){
+        if(i>j){
+            return 0;
         }
-        return dp[I][J];
+        if(i == j)return 1;
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = lps(i+1,j-1,s,dp)+2;
+        }
+        else{
+            return dp[i][j] = Math.max(lps(i+1,j,s,dp),lps(i,j-1,s,dp));
+        }
     }
 }
