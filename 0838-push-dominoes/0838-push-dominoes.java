e4f83f4011b73853f14f84e6032962817fcf0f23
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-04 18:07:41 +0530

    Time: 18 ms (82.95%), Space: 44.1 MB (51.65%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.java
+++ 0838-push-dominoes/0838-push-dominoes.java
@@ -1,41 +1,42 @@
-class Solution {
-    public String pushDominoes(String dominoes) {
+class Solution{
+    public String pushDominoes(String dominoes){
         dominoes = "L" + dominoes + "R";
         char[] arr = dominoes.toCharArray();
+        
         int pi = 0;
         
         for(int i = 1;i<arr.length;i++){
             if(arr[i] == 'L'){
-                if(arr[pi] == 'L'){
-                    for(int x = pi+1;x<i;x++){
-                        arr[x] = 'L';
+                if(arr[pi] == 'R'){
+                    int lo = pi+1;
+                    int hi = i-1;
+                    while(lo<hi){
+                        arr[lo] = 'R';
+                        arr[hi] = 'L';
+                        lo++;
+                        hi--;
                     }
                 }
-                else if(arr[pi] == 'R'){
-                    int li = pi+1;
-                    int ri = i-1;
-                    
-                    while(li<ri){
-                        arr[li] = 'R';
-                        arr[ri] = 'L';
-                        li++;
-                        ri--;
+                else if(arr[pi] == 'L'){
+                    for(int x = pi+1;x<i;x++){
+                        arr[x] = 'L';
                     }
                 }
                 pi = i;
             }
             else if(arr[i] == 'R'){
-                if(arr[pi] == 'L'){
-                    
-                }
-                else if(arr[pi] == 'R'){
+                if(arr[pi] == 'R'){
                     for(int x = pi+1;x<i;x++){
                         arr[x] = 'R';
                     }
                 }
+                else if(arr[pi] == 'L'){
+                    
+                }
                 pi = i;
             }
         }
+        
         StringBuilder sb = new StringBuilder();
         for(int i = 1;i<arr.length-1;i++){
             sb.append(arr[i]);
