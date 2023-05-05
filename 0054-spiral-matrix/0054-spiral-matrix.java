b656b167886cf033520a0f4386fa94bba51aff6a
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-05 08:31:21 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (44.22%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,40 +1,42 @@
-class Solution {
+class Solution{
     public List<Integer> spiralOrder(int[][] matrix){
         List<Integer> ans = new ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
         int dir = 0;
-        int top = 0;
-        int down = m-1;
         int left = 0;
         int right = n-1;
-        while(top<=down && left<=right){
-        if(dir == 0){
-            for(int i = left;i<=right;i++){
-                ans.add(matrix[top][i]);
+        int top = 0;
+        int down = m-1;
+        
+        while(left<=right && top<=down){
+            if(dir == 0){
+                for(int i = left;i<=right;i++){
+                    ans.add(matrix[top][i]);
+                }
+                top++;
             }
-            top++;
-        }
-        else if(dir == 1){
-            for(int i = top;i<=down;i++){
-                ans.add(matrix[i][right]);
+            else if(dir == 1){
+                for(int i = top;i<=down;i++){
+                    ans.add(matrix[i][right]);
+                }
+                right--;
             }
-            right--;
-        }
-        else if(dir == 2){
-            for(int i = right;i>=left;i--){
-                ans.add(matrix[down][i]);
+            else if(dir == 2){
+                for(int i = right;i>=left;i--){
+                    ans.add(matrix[down][i]);
+                }
+                down--;
             }
-            down--;
-        }
-        else if(dir == 3){
-            for(int i = down;i>=top;i--){
-                ans.add(matrix[i][left]);
+            else if(dir == 3){
+                for(int i = down;i>=top;i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
             }
-            left++;
+            dir = (dir+1)%4;
         }
-        dir = (dir+1)%4;
-    }
+        
         return ans;
-  }
+    }
 }
