b74f1a5e5b07032c24655e80290c3fa1f3e4b016
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-10 09:20:36 +0530

    Time: 2 ms (61.77%), Space: 40.8 MB (22.89%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,27 @@
+class Solution{
+    public int minAddToMakeValid(String s){
+        int ans = validParentheses(s);
+        return ans;
+    }
+    
+    public int validParentheses(String s){
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                st.push(ch);
+            }
+            else{
+                if(st.size() == 0 || st.peek() != '('){
+                    st.push(ch);
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return st.size();
+    }
+}
