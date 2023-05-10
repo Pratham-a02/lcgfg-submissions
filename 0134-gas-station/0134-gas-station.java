b1aab437df700176ed118700dbe119f554b4e7cf
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-10 22:11:58 +0530

    Time: 2 ms (99.58%), Space: 59.3 MB (45.82%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,22 +1,23 @@
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+class Solution{
+    public int canCompleteCircuit(int[] gas, int[] cost){
         int csum = 0;
         int osum = 0;
         int si = 0;
         
         for(int i = 0;i<gas.length;i++){
-            osum += (gas[i] - cost[i]);
-            csum += (gas[i] - cost[i]);
+            csum += (gas[i]-cost[i]);
+            osum += (gas[i]-cost[i]);
             
-            if(csum < 0){
-                csum = 0;
+            if(csum<0){
                 si = i+1;
+                csum = 0;
             }
         }
         
         if(osum<0 || si == gas.length){
             return -1;
         }
+        
         return si;
     }
 }
