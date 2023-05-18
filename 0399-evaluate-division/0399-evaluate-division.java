385bd71f00a316a0bc70803baf96053340103821
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-18 16:49:35 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (58.17%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,79 +1,80 @@
 class Solution{
     HashMap<String,String> parent;
-    HashMap<String,Double> mult;
-    public void addDSU(String x){
-        
+    HashMap<String,Double> multiplier;
+    
+    public void addToDSU(String x){
+        if(!parent.containsKey(x)){
             parent.put(x,x);
-            mult.put(x,1.0);
-        
+            multiplier.put(x,1.0);
+        }
     }
     
     public String find(String x){
-        if(parent.get(x) == x){
+        if(parent.get(x).equals(x)){
             return parent.get(x);
         }
         else{
-            String currparent = parent.get(x);
-            String finalparent = find(currparent);
+            String cp = parent.get(x);
+            String fp = find(cp);
             
-            parent.put(x,finalparent);
+            parent.put(x,fp);
+            multiplier.put(x,multiplier.get(x)*multiplier.get(cp));
             
-            mult.put(x,mult.get(currparent)*mult.get(x));
-                    return finalparent;
-
+            return fp;
         }
     }
     
-    
-    
-    
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries){
+    public double[] calcEquation(List<List<String>> equations,double[] values,List<List<String>>queries){
         parent = new HashMap<>();
-        mult = new HashMap<>();
+        multiplier = new HashMap<>();
         
-        for(List<String> eqn:equations){
-            addDSU(eqn.get(0));
-            addDSU(eqn.get(1));
+        for(List<String> eqn : equations){
+            addToDSU(eqn.get(0));
+            addToDSU(eqn.get(1));
         }
-        int i = 0;
+        int idx = 0;
         for(List<String> eqn : equations){
             String l0 = find(eqn.get(0));
-            String l1  = find(eqn.get(1));
-            
-            double m1 = mult.get(eqn.get(0));
-            double m2 = mult.get(eqn.get(1));
-
-            mult.put(l0,(m2*values[i])/m1);
-            parent.put(l0,l1);
-            i++;
-        }
-        i = 0;
-        double[] res = new double[queries.size()];
-        for(List<String> qu :queries){
-            if(parent.containsKey(qu.get(0)) == false || parent.containsKey(qu.get(1)) == false){
-                res[i] = -1;
-                i++;
-                continue;
-            }
-            
-            String l0 = find(qu.get(0));
-            String l1 = find(qu.get(1));
+            String l1 = find(eqn.get(1));
             
             if(l0.equals(l1) == false){
-                res[i] = -1;
-                i++;
+                //union
+                
+                double m1 = multiplier.get(eqn.get(0));
+                double m2 = multiplier.get(eqn.get(1));
+                
+                parent.put(l0,l1);
+                multiplier.put(l0,(m2*values[idx++]/m1));
+            }
+        }
+        
+        idx = 0;
+        double[] res = new double[queries.size()];
+        for(List<String> query : queries){
+            String q0 = query.get(0);
+            String q1 = query.get(1);
+            
+            if(!parent.containsKey(q0) || !parent.containsKey(q1)){
+                res[idx] = -1;
+                idx++;
                 continue;
             }
             
-            double m1 = mult.get(qu.get(0));
-            double m2 = mult.get(qu.get(1));
-            res[i] = m1/m2;
-            i++;
+            String l0 = find(q0);
+            String l1 = find(q1);
+            
+            if(l0.equals(l1) == false){
+                res[idx] = -1;
+                idx++;
+                continue;
+            }
+            
+            double m0 = multiplier.get(q0);
+            double m1 = multiplier.get(q1);
+            res[idx] = m0/m1;
+            idx++;
         }
+        
         return res;
     }
-    
-    
-    
-    
 }
