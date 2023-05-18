95bfc2dc08dccc4f7125389e111056f0b7165e22
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-18 18:05:33 +0530

    Time: 10 ms (59.19%), Space: 55.8 MB (38.74%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,19 +1,21 @@
-class Solution {
+class Solution{
     public int numEnclaves(int[][] grid){
         int m = grid.length;
         int n = grid[0].length;
+        boolean[][] vis = new boolean[m][n];
         
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(i*j == 0 || i == m-1 || j == n-1){
+                if((i*j == 0) || i == m-1 || j == n-1){
                     if(grid[i][j] == 1){
-                        dfs(i,j,m,n,grid);
+                        dfs(i,j,vis,grid);
                     }
                 }
             }
         }
         
         int count = 0;
+        
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(grid[i][j] == 1){
@@ -24,17 +26,18 @@ class Solution {
         return count;
     }
     
-    public void dfs(int currRow,int currCol,int m,int n,int[][] grid){
-        if(currRow>=m || currRow<0 || currCol >= n || currCol < 0 || grid[currRow][currCol] == 0){
+    public void dfs(int cr,int cc,boolean[][] vis,int[][] grid){
+        if(cr<0 || cr>=grid.length || cc<0 || cc>=grid[0].length || vis[cr][cc] == true || grid[cr][cc] == 0){
             return;
         }
         
-        grid[currRow][currCol] = 0;
+        vis[cr][cc] = true;
+        grid[cr][cc] = 0;
         
-        dfs(currRow-1,currCol,m,n,grid);
-        dfs(currRow,currCol+1,m,n,grid);
-        dfs(currRow+1,currCol,m,n,grid);
-        dfs(currRow,currCol-1,m,n,grid);
+        dfs(cr-1,cc,vis,grid);
+        dfs(cr,cc+1,vis,grid);
+        dfs(cr+1,cc,vis,grid);
+        dfs(cr,cc-1,vis,grid);
         
         return;
     }
