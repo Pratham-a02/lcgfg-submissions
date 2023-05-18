423673d1f5f254f0631ac696db73187a2ef6eccf
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-18 19:58:45 +0530

    Added solution - LeetHub

---
+++ Word Ladder I - GFG/word-ladder-i.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] wordList = new String[n];
+            for(int i = 0; i < n; i++){
+                wordList[i] = br.readLine().trim();
+            }
+            String startWord, targetWord;
+            startWord = br.readLine().trim();
+            targetWord = br.readLine().trim();
+            Solution obj = new Solution();
+            int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+            System.out.println(ans);
+       }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution{
+    public int wordLadderLength(String beginWord,String targetWord,String[] wordList){
+        if(Arrays.asList(wordList).contains(targetWord) == false){
+            return 0;
+        }
+        
+        ArrayDeque<String> qu = new ArrayDeque<>();
+        HashMap<String,Boolean> hm = new HashMap<>();
+        for(int i = 0;i<wordList.length;i++){
+            hm.put(wordList[i],false);
+        }
+        
+        qu.add(beginWord);
+        hm.put(beginWord,true);
+        int length = 1;
+        
+        while(qu.size()>0){
+            int csize = qu.size();
+            
+            for(int i = 0;i<csize;i++){
+                String currWord = qu.remove();
+                
+                if(currWord.equals(targetWord)){
+                    return length;
+                }
+                
+                addWord(currWord,hm,qu);
+            }
+            length++;
+        }
+        return 0;
+    }
+    
+    public void addWord(String currWord,HashMap<String,Boolean>hm,ArrayDeque<String> qu){
+        for(int i = 0;i<currWord.length();i++){
+            char[] arr = currWord.toCharArray();
+            
+            for(int j = 0;j<26;j++){
+                char c = (char)('a' + j);
+                
+                arr[i] = c;
+                
+                String s = String.valueOf(arr);
+                
+                if(hm.containsKey(s) && hm.get(s) == false){
+                    qu.add(s);
+                    hm.put(s,true);
+                }
+            }
+        }
+    }
+}
