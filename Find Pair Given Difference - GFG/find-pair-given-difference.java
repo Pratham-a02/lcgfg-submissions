4d5c99f7a87e1ca57ac71be4b17a14d7d38de724
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-20 12:31:43 +0530

    Added solution - LeetHub

---
+++ Find Pair Given Difference - GFG/find-pair-given-difference.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int l = sc.nextInt();
+            int n = sc.nextInt();
+            
+            int arr[] =  new int[l];
+            
+            for(int i = 0;i<l;i++)
+                arr[i] = sc.nextInt();
+            
+            Solution ob = new Solution();
+                
+            if(ob.findPair(arr, l, n)==true)
+                System.out.println(1);
+                
+            else
+                System.out.println(-1);    
+                
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+class Solution{
+    public boolean findPair(int arr[],int size,int n){
+        Arrays.sort(arr);
+        
+        int i = 0;
+        int j = 1;
+        
+        while(i<size && j<size){
+            if(arr[j] - arr[i] == n){
+                return true;
+            }
+            else if(arr[j] - arr[i] < n){
+                j++;
+            }
+            else if(arr[j] - arr[i] > n){
+                i++;
+                if(i == j){
+                    j++;
+                }
+            }
+        }
+        return false;
+    }
+}
