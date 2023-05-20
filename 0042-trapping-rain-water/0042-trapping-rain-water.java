d28a848184d5ccaaf3b7e14606736d1aaa9fccd5
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-20 09:57:08 +0530

    Time: 1 ms (98.69%), Space: 44.6 MB (7.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,30 @@
-class Solution{
-    public int trap(int[] height){
-        int n = height.length;
-        int[] mfl = new int[height.length];
-        int[] mfr = new int[height.length];
+class Solution {
+    public int trap(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int left=0;
+        int right=0;
+        int ans=0;
         
-        mfl[0] = height[0];
-        mfr[n-1] = height[n-1];
-        
-        for(int i = 1;i<height.length;i++){
-            mfl[i] = Math.max(height[i],mfl[i-1]);
-        }
-        
-        for(int i = n-2;i>=0;i--){
-            mfr[i] = Math.max(mfr[i+1],height[i]);
-        }
-        
-        int ans = 0;
-        
-        for(int i = 0;i<height.length;i++){
-            ans += (Math.min(mfr[i],mfl[i]) - height[i]);
+        while(l<=r){
+            if(height[l]<=height[r]){
+                if(height[l] >= left){
+                    left = height[l];
+                }
+                else{
+                    ans += left - height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r] >= right){
+                    right = height[r];
+                }
+                else{
+                    ans += right - height[r];
+                }
+                r--;
+            }
         }
         return ans;
     }
