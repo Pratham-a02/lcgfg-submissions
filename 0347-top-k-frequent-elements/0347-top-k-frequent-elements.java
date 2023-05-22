46954df1450a7ee58ddeb0d73c1996371fc6028c
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-05-22 06:48:23 +0530

    Time: 14 ms (67.33%), Space: 47.5 MB (5.35%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,9 +1,9 @@
 class Pair implements Comparable<Pair>{
-    int num;
+    int ele;
     int freq;
     
-    Pair(int num,int freq){
-        this.num = num;
+    Pair(int ele,int freq){
+        this.ele = ele;
         this.freq = freq;
     }
     
@@ -11,26 +11,32 @@ class Pair implements Comparable<Pair>{
         return this.freq - o.freq;
     }
 }
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        int[] ans = new int[k];
+class Solution{
+    public int[] topKFrequent(int[] nums, int k){
         PriorityQueue<Pair> pq = new PriorityQueue<>();
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int val:nums){
-            hm.put(val,hm.getOrDefault(val,0)+1);
+        
+        for(int num : nums){
+            hm.put(num,hm.getOrDefault(num,0)+1);
         }
         
-        for(int key:hm.keySet()){
-            Pair p = new Pair(key,hm.get(key));
-            pq.add(p);
+        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
+            int key = entry.getKey();
+            int val = entry.getValue();
+            
+            pq.add(new Pair(key,val));
+            
             if(pq.size()>k){
                 pq.remove();
             }
         }
+        
+        int[] ans = new int[k];
         int idx = 0;
+        
         while(pq.size()>0){
-            Pair p = pq.remove();
-            ans[idx++] = p.num;
+            Pair rem = pq.remove();
+            ans[idx++] = rem.ele;
         }
         return ans;
     }
