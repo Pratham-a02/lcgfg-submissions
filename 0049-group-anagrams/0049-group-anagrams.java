5f523179aef01945b1e0aa324a68dfa850f0ed61
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-07-24 08:52:36 +0530

    Time: 61 ms (9.18%), Space: 51.2 MB (5.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,24 +1,28 @@
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
+class Solution{
+    public List<List<String>> groupAnagrams(String[] strs){
+         List<List<String>> ans = new ArrayList<>();
+        
         HashMap<HashMap<Character,Integer>,List<String>> hm = new HashMap<>();
         
-        for(String str:strs){
-            HashMap<Character,Integer> hm1 = new HashMap<>();
-            for(int i = 0;i<str.length();i++){
-                hm1.put(str.charAt(i),hm1.getOrDefault(str.charAt(i),0)+1);
-            }
+        for(String str : strs){
+            HashMap<Character,Integer> curr = new HashMap<>();
             
-            if(hm.containsKey(hm1)){
-                List<String> curr = hm.get(hm1);
-                curr.add(str);
+            for(int i = 0;i<str.length();i++){
+                char ch = str.charAt(i);
+                
+                curr.put(ch,curr.getOrDefault(ch,0)+1);
             }
-            else{
-                List<String> curr = new ArrayList<>();
-                curr.add(str);
-                hm.put(hm1,curr);
+                if(hm.containsKey(curr)){
+                    List<String> al = hm.get(curr);
+                    al.add(str);
+                }
+                else{
+                    List<String> al = new ArrayList<>();
+                    al.add(str);
+                    hm.put(curr,al);
+                }
             }
-        }
+        
         
         for(List<String> curr :hm.values()){
             ans.add(curr);
