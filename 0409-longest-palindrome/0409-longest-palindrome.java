2c1ef09bf11bbc7db24327a863a58532621fc0b5
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-07-27 16:15:00 +0530

    Time: 2 ms (82.47%), Space: 40.7 MB (89.48%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,51 +1,50 @@
-class Solution {
-    public int longestPalindrome(String s) {
+class Solution{
+    public int longestPalindrome(String s){
         int[] lower = new int[26];
         int[] upper = new int[26];
         
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch - 97 >= 0){
-                lower[ch-97]++;
+            
+            if(ch-97>=0){
+                lower[ch-'a']++;
             }
             else{
-                upper[ch - 65]++;
+                upper[ch-'A']++;
             }
         }
         
         int ans = 0;
-        boolean foundOdd = false;
-        for(int i = 0;i<26;i++){
+        boolean found = false;
+        for(int i = 0;i<lower.length;i++){
             if(lower[i]%2 == 0){
                 ans += lower[i];
             }
             else{
-                if(foundOdd == false){
+                if(found == false){
+                    found = true;
                     ans += lower[i];
-                    foundOdd = true;
                 }
                 else{
-                    ans += lower[i] - 1;
+                    ans += lower[i]-1;
                 }
             }
         }
         
-        for(int i = 0;i<26;i++){
+        for(int i = 0;i<upper.length;i++){
             if(upper[i]%2 == 0){
                 ans += upper[i];
             }
             else{
-                if(foundOdd == false){
+                if(found == false){
+                    found = true;
                     ans += upper[i];
-                    foundOdd = true;
                 }
                 else{
-                    ans += upper[i] - 1;
-                    
+                    ans += upper[i]-1;
                 }
             }
         }
-        
         return ans;
     }
 }
