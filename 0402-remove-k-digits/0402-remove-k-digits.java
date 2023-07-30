9f9655fcc795e34c16df74cf4e417bbf69631d45
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-07-30 14:45:18 +0530

    Time: 81 ms (19.48%), Space: 45 MB (26.34%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,30 +1,33 @@
-class Solution{
-    public String removeKdigits(String num, int k){
-        int digits = num.length()-k;
-        if(num.length()<=k) return "0";
-        int top = -1;
-        
+class Solution {
+    public String removeKdigits(String num, int k) {
         char[] arr = num.toCharArray();
+        int digits = num.length()-k;
         
-        for(int i = 0;i<num.length();i++){
-            while(top>=0 && arr[top]>arr[i] && k>0){
-                top--;
+        int top = -1;
+        int start = 0;
+        
+        for(int i = 0;i<arr.length;i++){
+            while(top>= 0 && arr[top]>arr[i] && k>0){
                 k--;
+                top--;
             }
+            
             top++;
             arr[top] = arr[i];
         }
         
-        int start = 0;
         while(arr[start] == '0' && start<=top){
             start++;
         }
+        
         String ans = "";
         
         for(int i = start;i<digits;i++){
             ans += arr[i];
         }
-        if(ans.equals(""))return "0";
+        
+        if(ans.length() == 0) return "0";
+        
         return ans;
     }
 }
