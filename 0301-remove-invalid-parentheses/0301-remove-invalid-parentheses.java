31e6ca29863785b18d9dd4478393b39d3732f5c7
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-08-02 12:41:23 +0530

    Time: 504 ms (7.23%), Space: 54.6 MB (5.15%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -1,16 +1,10 @@
-class Solution{
-    public List<String> removeInvalidParentheses(String s){
-        List<String> ans = new ArrayList<>();
-        int count = getInvalid(s);
-        solve(s,count,ans,new HashSet<>());
-        return ans;
-    }
-    
-    public int getInvalid(String s){
+class Solution {
+    public int getCount(String s){
         Stack<Character> st = new Stack<>();
         
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
+            
             if(ch == '('){
                 st.push(ch);
             }
@@ -29,27 +23,33 @@ class Solution{
         return st.size();
     }
     
-    public void solve(String s,int count,List<String> ans,HashSet<String> vis){
-        if(vis.contains(s)){
+    public List<String> removeInvalidParentheses(String s) {
+        int count = getCount(s);
+        List<String> ans = new ArrayList<>();
+        solve(s,count,ans,new HashSet<>());
+        return ans;
+    }
+    
+    public void solve(String s,int count,List<String> ans,HashSet<String> hs){
+        if(hs.contains(s)){
             return;
-        }   
-        
+        }
         if(count == 0){
-            if(getInvalid(s) == 0){
-                if(!vis.contains(s)){
-                    vis.add(s);
+            if(getCount(s) == 0){
+                if(!hs.contains(s)){
+                    hs.add(s);
                     ans.add(s);
+                    return;   
                 }
             }
         }
         
-        vis.add(s);
+        hs.add(s);
         for(int i = 0;i<s.length();i++){
             String left = s.substring(0,i);
             String right = s.substring(i+1);
             
-            solve(left+right,count-1,ans,vis);
+            solve(left+right,count-1,ans,hs);
         }
-        return;
     }
 }
