447b24993660259c43aecd46ab1eff789bdbc33f
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-08-04 08:13:41 +0530

    Time: 10 ms (25.78%), Space: 44.4 MB (5.86%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,4 +1,4 @@
-class Solution{
+class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         return solve(s,wordDict,new HashMap<>());
     }
@@ -14,8 +14,8 @@ class Solution{
         
         for(int i = 0;i<s.length();i++){
             String substr = s.substring(0,i+1);
+            String next = s.substring(i+1);
             if(wordDict.contains(substr)){
-                String next = s.substring(i+1);
                 boolean res = solve(next,wordDict,hm);
                 hm.put(next,res);
                 if(res){
