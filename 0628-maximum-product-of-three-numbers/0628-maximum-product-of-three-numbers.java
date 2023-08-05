f2614f0ecced209a2cf20fa36a2640ded41d9ee7
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-08-05 14:22:50 +0530

    Time: 2 ms (99.56%), Space: 45 MB (59.51%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -8,15 +8,6 @@ class Solution {
         int max3 = Integer.MIN_VALUE;
         
         for(int num : nums){
-            if(num <= min1){
-                min2 = min1;
-                min1 = num;
-            }
-            else if(num<=min2){
-                min2 = num;
-            }
-            
-            
             if(num>=max1){
                 max3 = max2;
                 max2 = max1;
@@ -28,6 +19,14 @@ class Solution {
             }
             else if(num>=max3){
                 max3 = num;
+            }
+            
+            if(num <= min1){
+                min2 = min1;
+                min1 = num;
+            }
+            else if(num<=min2){
+                min2 = num;
             }
         }
         
