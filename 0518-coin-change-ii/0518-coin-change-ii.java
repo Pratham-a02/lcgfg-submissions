b7fecf634e733603175c4d6751788dc95d988925
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-08-11 07:53:37 +0530

    Time: 11 ms (24.16%), Space: 45.9 MB (39.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,14 +1,28 @@
 class Solution {
-    public int change(int Target, int[] coins) {
-        int[] dp = new int[Target+1];
-        dp[0] = 1;
-        for(int ele:coins){
-            for(int target = ele;target<=Target;target++){
-                if(target >= ele){
-                    dp[target] += dp[target-ele];//permutation_Infinite(coins,target-coins[i],dp);
-                }
-            }
+    public int change(int amount, int[] coins){
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
         }
-        return dp[Target];
+        return solve(0,amount,coins,dp);
+    }
+    
+    public int solve(int idx,int amount,int[] coins,int[][] dp){
+        if(amount == 0) return 1;
+        
+        if(idx>=coins.length) return 0;
+        
+        if(dp[idx][amount] != -1){
+            return dp[idx][amount];
+        }
+        
+        int taken = 0;
+        
+        if(amount - coins[idx] >= 0){
+            taken = solve(idx,amount-coins[idx],coins,dp);
+        }
+        int notTaken = solve(idx+1,amount,coins,dp);
+        
+        return dp[idx][amount] = taken + notTaken;
     }
 }
