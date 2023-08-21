b320a8adb8d6f30a07fea9de5c3b8ae0cdb5877d
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-08-21 11:00:56 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (74.55%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,27 +13,28 @@
  *     }
  * }
  */
+
 class Solution{
-    long prev = Long.MIN_VALUE;
-    public boolean isValidBST(TreeNode root) {
-        if(root == null){
-            return true;
-        }
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root){
+        if(root == null) return true;
         
         boolean la = isValidBST(root.left);
         if(la == false){
             return false;
         }
-        
-        if(prev >= root.val){
-            return false;
+        if(prev != null){
+            if(prev.val >= root.val){
+                return false;
+            }
         }
-        prev = root.val;
-        
+        prev = root;
         boolean ra = isValidBST(root.right);
         if(ra == false){
             return false;
         }
-        return true;
+        else{
+            return true;
+        }
     }
 }
