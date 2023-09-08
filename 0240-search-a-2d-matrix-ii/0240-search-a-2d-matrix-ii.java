c3d741a2534e0790e53d19831945bd43263e23ac
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-09-08 12:30:18 +0530

    Time: 5 ms (88.48%), Space: 48.9 MB (72.21%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -5,7 +5,8 @@ class Solution {
         int i = 0;
         int j = n-1;
         
-        while(i<m && j>=0){
+        while(i<m && j>=0){//j>=0 so that we do not move out of matrix
+            //i<m so that we do not go out of bound
             if(matrix[i][j] == target){
                 return true;
             }
