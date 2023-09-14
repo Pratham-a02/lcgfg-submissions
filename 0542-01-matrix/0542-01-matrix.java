8fe1533c096f551a78712c6aa539cf177f88f9f1
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-09-14 10:47:35 +0530

    Time: 20 ms (12.70%), Space: 45.6 MB (58.71%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -15,16 +15,16 @@ class Solution{
         int m = mat.length;
         int n = mat[0].length;
         
+        boolean[][] vis = new boolean[m][n];
         ArrayDeque<Pair> qu = new ArrayDeque<>();
-        for(int i = 0;i<mat.length;i++){
-            for(int j = 0;j<mat[0].length;j++){
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
                 if(mat[i][j] == 0){
                     qu.add(new Pair(i,j,0));
                 }
             }
         }
-        
-        boolean[][] vis = new boolean[m][n];
         
         while(qu.size()>0){
             Pair rem = qu.remove();
@@ -32,22 +32,22 @@ class Solution{
             if(vis[rem.x][rem.y] == true){
                 continue;
             }
-            
-            vis[rem.x][rem.y] = true;
             mat[rem.x][rem.y] = rem.dist;
+            vis[rem.x][rem.y] = true;
             
-            if(rem.x-1>=0){
+            if(rem.x - 1 >= 0){
                 qu.add(new Pair(rem.x-1,rem.y,rem.dist+1));
             }
             if(rem.y+1<n){
                 qu.add(new Pair(rem.x,rem.y+1,rem.dist+1));
             }
-            if(rem.x+1<m){
+            if(rem.x + 1 <m){
                 qu.add(new Pair(rem.x+1,rem.y,rem.dist+1));
             }
             if(rem.y-1>=0){
                 qu.add(new Pair(rem.x,rem.y-1,rem.dist+1));
             }
+            
         }
         return mat;
     }
