7fd9edff9716a54533d33feea14aeb94cde1ce37
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-09-26 09:23:44 +0530

    Time: 20 ms (85.60%), Space: 38.9 MB (80.80%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div526-beautiful-arrangement/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div526-beautiful-arrangement.java
@@ -0,0 +1,32 @@
+class Solution {
+    int ans;
+    public int countArrangement(int n) {
+        int[] nums = new int[n];
+        for(int i = 0;i<n;i++){
+            nums[i] = i+1;
+        }
+        ans = 0;
+        solve(0,nums);
+        return ans;
+    }
+    
+    public void solve(int j,int[] nums){
+        if(j>=nums.length){
+            ans++;
+        }
+        
+        for(int idx = j;idx<nums.length;idx++){
+            swap(idx,j,nums);
+            if((j+1)%nums[j] == 0 || nums[j]%(j+1) == 0){
+                solve(j+1,nums);
+            }
+            swap(idx,j,nums);
+        }
+    }
+    
+    public void swap(int i,int j,int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
