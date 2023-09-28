262f58b574e1af81bee2e06fa0a56873b2da9348
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-09-28 10:09:49 +0530

    Time: 2 ms (44.73%), Space: 6.5 MB (9.07%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long long size = 0;
+        
+        for(char &ch : s){
+            if(isdigit(ch)){
+                size = (size*(ch-'0'));
+            }
+            else{
+                size += 1;
+            }
+        }
+        
+        for(int i = s.size()-1;i>=0;i--){
+            k = k%size;
+            
+            if(k == 0 && isalpha(s[i])){
+                return string(1,s[i]);
+            }
+            
+            if(isalpha(s[i])){
+                size -= 1;
+            }
+            else{
+                size = size/(s[i]-'0');
+            }
+        }
+        return "";
+    }
+};
