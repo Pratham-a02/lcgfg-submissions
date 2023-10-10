3c02abb318532ad285699febb228409f01a86a0b
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-10-10 22:10:44 +0530

    Added solution - LeetHub

---
+++ Longest Path in a matrix - GFG/longest-path-in-a-matrix.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S1 = br.readLine().trim().split(" ");
+            int n  = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            int[][] matrix = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S2 = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    matrix[i][j] = Integer.parseInt(S2[j]);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.longestIncreasingPath(matrix);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution{
+    public int longestIncreasingPath(int[][] matrix){
+        int max = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                max = Math.max(max,solve(i,j,matrix,dp));
+            }
+        }
+        return max;
+    }
+    
+    public int solve(int i,int j,int[][] matrix,int[][] dp){
+        if(i<0 || j<0 || i>=matrix.length || j>=matrix[0].length){
+            return 0;
+        }
+        
+        if(dp[i][j] > 0){
+            return dp[i][j];
+        }
+        
+        int currMax = 1;
+        if(i-1>=0 && matrix[i-1][j]<matrix[i][j]){
+            currMax = Math.max(currMax,1 + solve(i-1,j,matrix,dp));
+        }
+        if(j+1<matrix[0].length && matrix[i][j+1] < matrix[i][j]){
+            currMax = Math.max(currMax,1 + solve(i,j+1,matrix,dp));
+        }
+        if(i+1<matrix.length && matrix[i+1][j] < matrix[i][j]){
+            currMax = Math.max(currMax,1 + solve(i+1,j,matrix,dp));
+        }
+        if(j-1 >= 0 && matrix[i][j-1] < matrix[i][j]){
+            currMax = Math.max(currMax,1 + solve(i,j-1,matrix,dp));
+        }
+        
+        return dp[i][j] = currMax;
+    }
+}
