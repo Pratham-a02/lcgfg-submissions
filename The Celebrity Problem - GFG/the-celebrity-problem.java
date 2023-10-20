3222d2e29b749f57993aede39916367d70e36257
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-10-20 11:19:26 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -25,8 +25,9 @@ class GFG{
     } 
 } 
 // } Driver Code Ends
+
 class Solution{ 
-    int celebrity(int M[][], int n){
+    int celebrity(int[][] M,int n){
     	Stack<Integer> st = new Stack<>();
     	for(int i = 0;i<n;i++){
     	    st.push(i);
@@ -36,26 +37,25 @@ class Solution{
     	    int i = st.pop();
     	    int j = st.pop();
     	    
-    	    if(M[i][j] == 1){
-    	        st.push(j);
-    	    }
-    	    else{
+    	    if(M[i][j] == 0 && M[j][i] == 1){
     	        st.push(i);
     	    }
+    	    else if(M[j][i] == 0 && M[i][j] == 1){
+    	        st.push(j);
+    	    }
     	}
-    	
     	if(st.size() == 0){
     	    return -1;
     	}
+    	int cele = st.pop();
     	
-    	int pot = st.pop();
     	for(int i = 0;i<n;i++){
-    	    if(i!=pot){
-    	        if(M[pot][i] == 1 || M[i][pot] == 0){
+    	    if(i != cele){
+    	        if(M[i][cele] == 0 || M[cele][i] == 1){
     	            return -1;
     	        }
     	    }
     	}
-    	return pot;
+    	return cele;
     }
 }
