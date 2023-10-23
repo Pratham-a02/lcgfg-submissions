3c57f1192959fa34b9d1d469384036a9afb21a3a
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-10-23 15:14:17 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.java
+++ Rotten Oranges - GFG/rotten-oranges.java
@@ -32,57 +32,55 @@ class Pair{
     int x;
     int y;
     int t;
+    
     Pair(int x,int y,int t){
         this.x = x;
         this.y = y;
         this.t = t;
     }
 }
-
 class Solution{
-    
     public int orangesRotting(int[][] grid){
-        ArrayDeque<Pair> qu = new ArrayDeque<>();
         int fresh = 0;
+        ArrayDeque<Pair> qu = new ArrayDeque<>();
         int time = 0;
-        boolean[][] vis = new boolean[grid.length][grid[0].length];
         for(int i = 0;i<grid.length;i++){
             for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j] == 2){
                     qu.add(new Pair(i,j,0));
-                }else if(grid[i][j] == 1){ 
+                }
+                else if(grid[i][j] == 1){
                     fresh++;
                 }
             }
         }
-        
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
         while(qu.size()>0){
             Pair rem = qu.remove();
-            if(vis[rem.x][rem.y] == true){
-                continue;
-            }
+            if(vis[rem.x][rem.y] == true) continue;
+            
             vis[rem.x][rem.y] = true;
             if(rem.t>time){
                 time = rem.t;
             }
-            if(grid[rem.x][rem.y] == 1){
-                fresh--;
-            }
             
-            addNbr(rem.x-1,rem.y,vis,grid,qu,rem.t);
-            addNbr(rem.x,rem.y+1,vis,grid,qu,rem.t);
-            addNbr(rem.x+1,rem.y,vis,grid,qu,rem.t);
-            addNbr(rem.x,rem.y-1,vis,grid,qu,rem.t);
+            if(grid[rem.x][rem.y] == 1) fresh--;
             
+            addNbr(rem.x-1,rem.y,rem.t,vis,grid,qu);
+            addNbr(rem.x,rem.y+1,rem.t,vis,grid,qu);
+            addNbr(rem.x+1,rem.y,rem.t,vis,grid,qu);
+            addNbr(rem.x,rem.y-1,rem.t,vis,grid,qu);
         }
+        
         if(fresh == 0){
             return time;
-        }else{
+        }
+        else{
             return -1;
         }
     }
     
-    public void addNbr(int x,int y,boolean[][] vis,int[][] grid,ArrayDeque<Pair>qu,int time){
+    public static void addNbr(int x,int y,int time,boolean[][] vis,int[][] grid,ArrayDeque<Pair> qu){
         if(x<0 || y<0 || x>=grid.length || y>=grid[0].length || vis[x][y] == true || grid[x][y] == 0){
             return;
         }
