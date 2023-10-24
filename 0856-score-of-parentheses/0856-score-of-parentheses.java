334bb7475e78935d23f81373a736cb4cb375e70f
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-10-24 21:25:43 +0530

    Time: 1 ms (67.52%), Space: 39.9 MB (93.81%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,33 @@
+class Solution{
+    public int scoreOfParentheses(String s){
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                st.push(-1);
+            }
+            else{
+                if(st.peek() == -1){
+                    st.pop();
+                    st.push(1);
+                }
+                else{
+                    int val = 0;
+                    while(st.peek() != -1){
+                        val += st.pop();
+                    }
+                    
+                    st.pop();
+                    st.push(2*val);
+                }
+            }
+        }
+        int ans = 0;
+        while(st.size()>0){
+            ans += st.pop();
+        }
+        return ans;
+    }
+}
