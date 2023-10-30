255a66ef89cbb4e8af8d7815e32b699a0c6b4ecb
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-10-30 08:38:54 +0530

    Time: 1 ms (100.00%), Space: 41 MB (63.77%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,37 +1,29 @@
-class Solution {
-    public String removeDuplicateLetters(String s) {
-        Stack<Character> st = new Stack<>();
-        int[] freq = new int[26];
-        
+class Solution{
+    public String removeDuplicateLetters(String s){
+        int[] lastIndex = new int[26];
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
-            freq[ch-'a']++;
+            lastIndex[ch-'a'] = i;
         }
         
+        StringBuilder sb = new StringBuilder();
         boolean[] vis = new boolean[26];
         
         for(int i = 0;i<s.length();i++){
             char ch = s.charAt(i);
+            if(vis[ch-'a'] == true) continue;
             
-            if(vis[ch-'a'] == true){
-                freq[ch-'a']--;
-                continue;
+            while(sb.length()>0 && ch<sb.charAt(sb.length()-1) && lastIndex[sb.charAt(sb.length()-1) - 'a'] > i){
+                vis[sb.charAt(sb.length()-1)-'a'] = false;
+                sb.deleteCharAt(sb.length()-1);
+                
             }
-            while(st.size()>0 && st.peek()>ch && freq[st.peek()-'a']>0){
-                vis[st.peek()-'a'] = false;
-                st.pop();
-            }
+           
+                sb.append(ch);                            
+                vis[ch-'a'] = true;             
             
-            vis[ch-'a'] = true;
-            freq[ch-'a']--;
-            st.push(ch);
         }
-        
-        StringBuilder sb = new StringBuilder();
-        while(st.size()>0){
-            char ch = st.pop();
-            sb.insert(0,ch);
-        }
-        return sb.toString();
+                                                                            
+        return sb.toString();                                                           
     }
 }
