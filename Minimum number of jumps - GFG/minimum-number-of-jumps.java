9e6adc4c160b4a9a31375520477074edb2b0d858
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-11-23 10:19:05 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.java
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.java
@@ -32,31 +32,30 @@ class Solution{
         if(n == 1){
             return 0;
         }
-        
         if(arr[0] == 0){
             return -1;
         }
         
-        int maxReach = arr[0];
         int jump = 1;
+        int maxReach = arr[0];
         int steps = arr[0];
         
-        for(int i = 1;i<n;i++){
+        for(int i = 1;i<arr.length;i++){
             if(i == n-1){
                 return jump;
             }
             
             steps--;
             maxReach = Math.max(maxReach,i+arr[i]);
-            
             if(steps == 0){
+                jump++;
                 if(i>=maxReach){
                     return -1;
                 }
                 steps = maxReach - i;
-                jump++;
             }
         }
-        return jump;
+        
+        return -1;
     }
 }
