47a52524d3ad08130843d2bd3d3873f52a6dd4f2
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2023-12-26 16:41:11 +0530

    Time: 1 ms (76.43%), Space: 41.6 MB (6.05%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div925-long-pressed-name/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div925-long-pressed-name.java
@@ -0,0 +1,20 @@
+class Solution{
+    public boolean isLongPressedName(String name,String typed){
+        int i = 0;
+        int j = 0;
+        
+        while(i<typed.length()){
+            if(j<name.length() && name.charAt(j) == typed.charAt(i)){
+                i++;
+                j++;
+            }
+            else if(i>0 && typed.charAt(i) == typed.charAt(i-1)){
+                i++;
+            }
+            else{
+                return false;
+            }
+        }
+        return (j == name.length());
+    }
+}
