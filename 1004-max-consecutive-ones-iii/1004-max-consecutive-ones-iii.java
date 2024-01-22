232f7f3e0c0515fce55573bd98ee14da2f99d4a8
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-01-22 22:30:03 +0530

    Time: 3 ms (79.46%), Space: 47.5 MB (8.53%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,23 +3,18 @@ class Solution {
         int max = 0;
         int left = 0;
         int count = 0;
-        int currlen = 0;
         for(int right = 0;right<nums.length;right++){
             if(nums[right] == 0){
                 count++;
             }
-            if(count<=k){
-                currlen = right-left+1;
-            }
-            
+
             while(count>k){
                 if(nums[left] == 0){
                     count--;
                 }
                 left++;
             }
-            
-            max = Math.max(max,currlen);
+            max = Math.max(max,right-left+1);
         }
         return max;
     }
