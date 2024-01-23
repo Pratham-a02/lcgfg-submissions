ec21e1bd1ed79faed8ae0e65674f3a6211301a0f
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-01-23 22:10:28 +0530

    Time: 32 ms (72.48%), Space: 48.4 MB (92.93%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,28 +1,26 @@
-class Solution {
-    class Pair implements Comparable<Pair>{
-        int li;
-        int idx;
-        int val;
-        
-        Pair(int li,int idx,int val){
-            this.li = li;
-            this.idx = idx;
-            this.val = val;
-        }
-        
-        public int compareTo(Pair o){
-            return this.val - o.val;
-        }
-    }
+class Pair implements Comparable<Pair>{
+    int li;
+    int idx;
+    int val;
     
+    Pair(int li,int idx,int val){
+        this.li = li;
+        this.idx = idx;
+        this.val = val;
+    }
+    public int compareTo(Pair o){
+        return this.val - o.val;
+    }
+}
+
+class Solution{
     public int[] smallestRange(List<List<Integer>> nums){
-        int max = Integer.MIN_VALUE;
         PriorityQueue<Pair> pq = new PriorityQueue<>();
+        int max = Integer.MIN_VALUE;
         for(int i = 0;i<nums.size();i++){
             pq.add(new Pair(i,0,nums.get(i).get(0)));
             max = Math.max(max,nums.get(i).get(0));
         }
-        
         
         int rbeg = 0;
         int rend = 0;
@@ -30,26 +28,22 @@ class Solution {
         
         while(pq.size() == nums.size()){
             Pair rem = pq.remove();
-            int crsize = max - rem.val;
-            
-            if(crsize<rsize){
-                rsize = crsize;
+            int currSize = max - rem.val;
+            if(currSize<rsize){
                 rbeg = rem.val;
                 rend = max;
+                rsize = currSize;
             }
             
             if(rem.idx<nums.get(rem.li).size()-1){
                 rem.idx++;
                 rem.val = nums.get(rem.li).get(rem.idx);
                 pq.add(rem);
+                max = Math.max(max,nums.get(rem.li).get(rem.idx));
                 
-                if(rem.val>max){
-                    max = rem.val;
-                }
             }
-            
-            
         }
+        
         return new int[]{rbeg,rend};
     }
 }
