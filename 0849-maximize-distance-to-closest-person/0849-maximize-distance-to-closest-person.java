1c4b3a27402c19351397e4c4548ee9a1233ea175
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-01-23 21:19:32 +0530

    Time: 2 ms (98.77%), Space: 46.8 MB (11.32%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.java
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.java
@@ -1,24 +1,21 @@
-class Solution{
-    public int maxDistToClosest(int[] seats){
+class Solution {
+    public int maxDistToClosest(int[] seats) {
         int j = -1;
         int res = 0;
         
         for(int i = 0;i<seats.length;i++){
             if(seats[i] == 1){
-            int ans = 0;
-            
-            if(j<0){
-                ans = i;
+                if(j<0){
+                    res = i;
+                }
+                else{
+                    res = Math.max(res,(i-j)/2);
+                }
+                j = i;
             }
-            else{
-                ans = (i-j)/2;
-            }
-            
-            res = Math.max(res,ans);
-            j = i;
         }
-        }
-        res = Math.max(res,seats.length-1-j);
+        
+        res = Math.max(res,seats.length-j-1);
         return res;
     }
 }
