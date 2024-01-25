cb894bfd7877820275e1d43c338fde0ade384ef1
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-01-25 09:23:27 +0530

    Time: 2 ms (66.92%), Space: 57 MB (78.08%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div775-global-and-local-inversions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div775-global-and-local-inversions.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div775-global-and-local-inversions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div775-global-and-local-inversions.java
@@ -1,73 +1,10 @@
-class Solution{
-    int gi;
-    public boolean isIdealPermutation(int[] nums){
-        return localInversion(nums) == globalInversion(nums);
-    }
-    
-    public int localInversion(int[] nums){
-        int count = 0;
-        for(int i = 0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]){
-                count++;
+class Solution {
+    public boolean isIdealPermutation(int[] nums) {
+        for(int i = 0;i<nums.length;i++){
+            if(Math.abs(nums[i]-i)>1){
+                return false;
             }
         }
-        return count;
-    }
-    
-    public int globalInversion(int[] nums){
-        gi = 0;
-        solve(0,nums.length-1,nums);
-        return gi;
-    }
-    
-    public int[] solve(int lo,int hi,int[] nums){
-        if(lo == hi){
-            int[] ba = new int[1];
-            ba[0] = nums[lo];
-            return ba;
-        }
-        
-        int mid = (lo+hi)/2;
-        
-        int[] la = solve(lo,mid,nums);
-        int[] ra = solve(mid+1,hi,nums);
-        
-        int[] fa = mergeSort(la,ra);
-        return fa;
-    }
-    
-    public int[] mergeSort(int[] a1,int[] a2){
-        int[] arr = new int[a1.length+a2.length];
-        
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        
-        while(i<a1.length && j<a2.length){
-            if(a1[i]<a2[j]){
-                arr[k] = a1[i];
-                i++;
-                k++;
-            }
-            else if(a1[i]>a2[j]){
-                gi+=(a1.length-i);
-                arr[k] = a2[j];
-                k++;
-                j++;
-            }
-        }
-        
-        while(i<a1.length){
-            arr[k] = a1[i];
-            i++;
-            k++;
-        }
-        while(j<a2.length){
-            arr[k] = a2[j];
-            j++;
-            k++;
-        }
-        
-        return arr;
+        return true;
     }
 }
