84a6f09e49a9714f6dc4441762502ce2b1ebc227
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-01-25 14:54:38 +0530

    Time: 14 ms (30.56%), Space: 47 MB (7.17%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,10 @@
+class Solution{
+    public int arrayPairSum(int[] nums){
+        Arrays.sort(nums);
+        int ans = 0;
+        for(int i = 0;i<nums.length-1;i+=2){
+            ans += Math.min(nums[i],nums[i+1]);
+        }
+        return ans;
+    }
+}
