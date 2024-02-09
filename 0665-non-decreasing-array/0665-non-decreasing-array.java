03b353c17482b443f69d9c9db4e34f745ece11b7
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-02-09 10:10:49 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (57.66%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -0,0 +1,22 @@
+class Solution{
+    public boolean checkPossibility(int[] nums){
+        int count = 0;
+        
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                count++;
+                if(count>1){
+                    return false;
+                }
+                
+                if((i == 1) || nums[i]>=nums[i-2]){
+                    nums[i-1] = nums[i];
+                }
+                else{
+                    nums[i] = nums[i-1];
+                }
+            }
+        }
+        return true;
+    }
+}
