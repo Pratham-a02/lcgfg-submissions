3cc061f778d55d5e3567ff0962cb1e4d9928c512
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-02-24 12:38:40 +0530

    Time: 4 ms (95.86%), Space: 45.4 MB (96.56%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,10 +1,13 @@
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+class Solution{
+    public List<Integer> findClosestElements(int[] arr,int k,int x){
         int lo = 0;
         int hi = arr.length-1;
+        List<Integer> ans = new ArrayList<>();
+        
         while(hi-lo>=k){
             int val1 = Math.abs(arr[lo]-x);
             int val2 = Math.abs(arr[hi]-x);
+            
             if(val1>val2){
                 lo++;
             }
@@ -12,7 +15,7 @@ class Solution {
                 hi--;
             }
         }
-        List<Integer> ans = new ArrayList<>();
+        
         for(int i = lo;i<=hi;i++){
             ans.add(arr[i]);
         }
