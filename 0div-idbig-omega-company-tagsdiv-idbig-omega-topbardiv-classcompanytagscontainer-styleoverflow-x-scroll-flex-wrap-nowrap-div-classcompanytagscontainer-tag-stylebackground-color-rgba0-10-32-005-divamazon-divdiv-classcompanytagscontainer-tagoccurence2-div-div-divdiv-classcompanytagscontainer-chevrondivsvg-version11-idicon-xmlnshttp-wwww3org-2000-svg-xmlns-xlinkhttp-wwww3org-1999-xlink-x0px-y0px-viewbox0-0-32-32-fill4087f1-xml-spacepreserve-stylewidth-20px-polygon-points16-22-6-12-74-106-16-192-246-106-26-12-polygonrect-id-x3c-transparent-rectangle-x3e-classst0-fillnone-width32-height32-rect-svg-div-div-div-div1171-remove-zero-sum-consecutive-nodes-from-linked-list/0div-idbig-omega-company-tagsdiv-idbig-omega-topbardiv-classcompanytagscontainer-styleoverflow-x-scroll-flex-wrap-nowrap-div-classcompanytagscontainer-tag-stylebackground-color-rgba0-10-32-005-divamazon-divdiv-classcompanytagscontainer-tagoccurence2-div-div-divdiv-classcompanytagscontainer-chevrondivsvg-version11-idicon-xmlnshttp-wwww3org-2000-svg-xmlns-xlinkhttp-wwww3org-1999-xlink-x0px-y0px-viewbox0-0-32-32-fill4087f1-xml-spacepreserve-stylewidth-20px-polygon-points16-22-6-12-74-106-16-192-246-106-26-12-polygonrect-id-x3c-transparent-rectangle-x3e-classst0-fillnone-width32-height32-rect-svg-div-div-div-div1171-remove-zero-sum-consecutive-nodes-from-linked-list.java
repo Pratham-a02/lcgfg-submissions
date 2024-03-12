e9b5f711b57fd5efb30af55a397a446ac28a428e
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-03-12 09:10:19 +0530

    Time: 2 ms (89.13%), Space: 44.2 MB (40.71%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1171-remove-zero-sum-consecutive-nodes-from-linked-list/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1171-remove-zero-sum-consecutive-nodes-from-linked-list/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -1,38 +1,31 @@
-/*public class ListNode{
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(){}
- *     ListNode(int val){ this.val = val;}
- *     ListNode(int val,ListNode next){this.val = val;this.next = next;}}*/
-
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution{
     public ListNode removeZeroSumSublists(ListNode head){
         HashMap<Integer,ListNode> hm = new HashMap<>();
-        ListNode dummy = new ListNode(-1);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
-        hm.put(0,dummy);
-        int ps = 0;
-        while(head != null){
-            ps += head.val;
-            
-            if(hm.containsKey(ps)){
-                ListNode p = hm.get(ps);
-                ListNode start = p;
-                int currps = ps;
-                while(start != head){
-                    start = start.next;
-                    currps += start.val;
-                    if(start != head){
-                        hm.remove(currps);
-                    }
-                }
-                p.next = start.next;
-            }
-            else{
-                hm.put(ps,head);
-            }
-            head = head.next;
+        
+        int sum = 0;
+        for(ListNode p = dummy;p!= null;p = p.next){
+            sum += p.val;
+            hm.put(sum,p);
         }
+        
+        sum = 0;
+        for(ListNode p = dummy;p!= null;p = p.next){
+            sum += p.val;
+            p.next = hm.get(sum).next;
+        }
+        
         return dummy.next;
     }
 }
