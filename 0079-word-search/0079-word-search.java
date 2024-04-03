f38e0baf3923ad0e2986aede7120d09294dd62da
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-04-03 08:20:01 +0530

    Time: 153 ms (48.81%), Space: 41.3 MB (84.84%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,8 +1,11 @@
-class Solution {
-    public boolean exist(char[][] board, String word) {
-        for(int i = 0;i<board.length;i++){
-            for(int j = 0;j<board[0].length;j++){
-                if(board[i][j] == word.charAt(0) && dfs(i,j,0,board,word)){
+class Solution{
+    public boolean exist(char[][] board,String word){
+        int m = board.length;
+        int n = board[0].length;
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(board[i][j] == word.charAt(0) && solve(i,j,m,n,0,board,word)){
                     return true;
                 }
             }
@@ -10,22 +13,20 @@ class Solution {
         return false;
     }
     
-    public boolean dfs(int i,int j,int count,char[][] board,String word){
-        if(count == word.length()){
+    public boolean solve(int x,int y,int m,int n,int idx,char[][] board,String word){
+        if(idx == word.length()){
             return true;
         }
-        
-        if(i<0 ||j<0 || i>=board.length || j>=board[0].length || board[i][j] != word.charAt(count)){
+        if(x<0 || y<0 || x>=m || y>=n || word.charAt(idx) != board[x][y]){
             return false;
         }
         
-        char temp = board[i][j];
-        board[i][j] = '*';
+        char temp = board[x][y];
+        board[x][y] = '*';
         
-        boolean found = (dfs(i-1,j,count+1,board,word) || dfs(i+1,j,count+1,board,word) || dfs(i,j-1,count+1,board,word) || dfs(i,j+1,count+1,board,word));
+        boolean flag = solve(x-1,y,m,n,idx+1,board,word) || solve(x,y+1,m,n,idx+1,board,word) || solve(x+1,y,m,n,idx+1,board,word) || solve(x,y-1,m,n,idx+1,board,word);
         
-        board[i][j] = temp;
-        return found;
-        
+        board[x][y] = temp;
+        return flag;
     }
 }
