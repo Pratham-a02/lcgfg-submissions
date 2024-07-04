53479124a933f5c4d5cdb6d0021ea9ac0aa5bf14
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-07-04 21:30:17 +0530

    Time: 14 ms (63.64%), Space: 46.4 MB (5.30%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div451-sort-characters-by-frequency/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div451-sort-characters-by-frequency.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div451-sort-characters-by-frequency/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div451-sort-characters-by-frequency.java
@@ -1,53 +1,30 @@
-class Pair implements Comparable<Pair>{
-    int freq;
-    char c;
-    
-    Pair(int freq,char c){
-        this.freq = freq;
-        this.c = c;
-    }
-    
-    public int compareTo(Pair o){
-        return o.freq - this.freq;
-    }
-}
 class Solution {
     public String frequencySort(String s) {
-        //hashmap for counting the frequency of each character in string s
-        HashMap<Character , Integer> hm = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            if(hm.containsKey(s.charAt(i))){
-                int old = hm.get(s.charAt(i));
-                hm.put(s.charAt(i) , old+1);
+        List<Character>[] arr = new List[s.length()+1];
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(char ch:s.toCharArray()){
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+        }
+        
+        for(Map.Entry<Character,Integer> entry:hm.entrySet()){
+            char ch = entry.getKey();
+            int freq = entry.getValue();
+            if(arr[freq] == null){
+                arr[freq] = new ArrayList<>();
             }
-            else{
-                hm.put(s.charAt(i) , 1);
+            arr[freq].add(ch);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length-1;i>=0;i--){
+            if(arr[i] != null){
+                for(char ch:arr[i]){
+                    for(int j = 0;j<i;j++){
+                        sb.append(ch);
+                    }
+                }
             }
         }
         
-        // creating a priority queue with max heap which will store the elements in decreasing order of frequency
-        PriorityQueue<Pair> pq =  new PriorityQueue<>();
-        for(Map.Entry<Character , Integer> map : hm.entrySet()){
-            char key = map.getKey();
-            int val = map.getValue();
-            pq.add(new Pair(val , key));
-        }
-        
-        //now we will simply get the resultant string get done by 
-        //removing the top most value from queue and adding it into the res for its frequency times
-        String res = "";
-        while(pq.size() > 0){
-            // int val = pq.peek().getKey();
-            // char key = pq.peek().getValue();
-            Pair p = pq.remove();
-            int val = p.freq;
-            char key = p.c;
-            while(val > 0){
-                res += key;
-                val--;
-            }
-            // pq.remove();
-        }
-        return res;
+        return sb.toString();
     }
 }
