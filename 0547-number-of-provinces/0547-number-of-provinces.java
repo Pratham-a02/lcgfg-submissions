c5f73c6de0227e7ff98ae1b6a8c2f3809b445c71
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-07-20 17:00:33 +0530

    Time: 2 ms (50.43%), Space: 47.9 MB (22.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,40 +1,83 @@
 class Solution {
+    int[] parent;
+    int[] rank;
     public int findCircleNum(int[][] isConnected) {
-        int count = 0;
-        int n = isConnected.length;
-        boolean[] vis = new boolean[n];
         
-        List<List<Integer>> al = new ArrayList<>();
-        for(int i = 0;i<n;i++){
-            al.add(new ArrayList<>());
+        int n = isConnected.length;
+        parent = new int[n];
+        rank = new int[n];
+        
+        for(int i = 0;i<parent.length;i++){
+            parent[i] = i;
+            rank[i] = 0;
         }
+        
         
         for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
-                if(isConnected[i][j] == 1){
-                    if(i!=j){
-                        al.get(i).add(j);
+                if(i != j){
+                    if(isConnected[i][j] == 1){
+                        int iLead = find(i);
+                        int jLead = find(j);
+                        
+                        if(iLead != jLead){
+                            union(iLead,jLead);
+                        }
                     }
                 }
             }
         }
-        for(int i = 0;i<al.size();i++){
-            if(!vis[i]){
-                count++;
-                dfs(i,vis,al);
+        
+        int ans = 0;
+        for(int i = 0;i<parent.length;i++){
+            if(parent[i] == i){
+                ans++;
             }
         }
-        return count;
+        
+        return ans;
+        //  0 1 2
+        //0 1 1 0
+        //1 1 1 0
+        //2 0 0 1
+        
+        
+        /*
+         0 1 2  
+     0   0 1 2     r*n + c
+     1   3 4 5
+     2   6 7 8
+        a
+        /\
+        b c
+       
+        0 1 2 3 4 5 6 7 8
+        */
     }
     
-    public void dfs(int src,boolean[] vis,List<List<Integer>> graph){
-        vis[src] = true;
-        
-        for(int nbr:graph.get(src)){
-            if(!vis[nbr]){
-                dfs(nbr,vis,graph);
-            }
+    public int find(int x){
+        if(parent[x] == x){
+            return parent[x];
         }
-        return;
+        else{
+            int cp = parent[x];
+            int myPar = find(cp);
+            return parent[x] = myPar;
+        }
+    }
+    
+    public void union(int x,int y){
+        if(rank[x] > rank[y]){
+            parent[y] = x;
+            rank[x]++;
+        }
+        else if(rank[y]>rank[x]){
+            parent[x] = y;
+            rank[y]++;
+        }
+        else{
+            parent[y] = x;
+            rank[x]++;
+        }
     }
 }
