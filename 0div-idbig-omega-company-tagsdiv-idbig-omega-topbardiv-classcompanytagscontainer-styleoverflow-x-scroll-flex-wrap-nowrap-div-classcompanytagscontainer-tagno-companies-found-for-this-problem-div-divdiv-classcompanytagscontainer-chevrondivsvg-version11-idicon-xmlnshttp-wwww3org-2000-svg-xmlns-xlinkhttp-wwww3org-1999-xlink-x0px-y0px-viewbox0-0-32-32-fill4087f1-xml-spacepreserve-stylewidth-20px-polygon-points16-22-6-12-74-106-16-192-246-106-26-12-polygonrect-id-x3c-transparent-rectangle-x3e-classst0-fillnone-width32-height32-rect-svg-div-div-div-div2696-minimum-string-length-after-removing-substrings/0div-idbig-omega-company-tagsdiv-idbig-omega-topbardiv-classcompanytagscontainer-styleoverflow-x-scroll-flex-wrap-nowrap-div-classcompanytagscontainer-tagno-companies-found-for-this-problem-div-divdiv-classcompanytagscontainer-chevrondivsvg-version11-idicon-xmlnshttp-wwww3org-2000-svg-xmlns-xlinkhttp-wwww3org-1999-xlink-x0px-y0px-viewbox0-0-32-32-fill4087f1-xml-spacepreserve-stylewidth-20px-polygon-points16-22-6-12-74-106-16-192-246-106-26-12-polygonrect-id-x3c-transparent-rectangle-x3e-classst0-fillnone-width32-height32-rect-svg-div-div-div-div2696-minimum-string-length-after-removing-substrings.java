1387509a012e2a315c2f8cf3db84ee1c467565c9
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-10-07 19:27:59 +0530

    Time: 4 ms (85.00%), Space: 42.9 MB (91.72%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2696-minimum-string-length-after-removing-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            if(st.size()>0){
+                if(s.charAt(i) == 'B' && st.peek() == 'A'){
+                    st.pop();
+                }
+                else if(s.charAt(i) == 'D' && st.peek() == 'C'){
+                    st.pop();
+                }
+                else{
+                    st.push(s.charAt(i));
+                }
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        return st.size();
+    }
+}
