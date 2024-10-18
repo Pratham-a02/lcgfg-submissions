4049444ab1e37f209a689941f507ad5e1be923f7
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-10-18 22:53:28 +0530

    Time: 270 ms (18.52%), Space: 42.5 MB (43.39%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div395-longest-substring-with-at-least-k-repeating-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div395-longest-substring-with-at-least-k-repeating-characters/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,29 +1,34 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        int[] freq = new int[26];
-        for(char ch:s.toCharArray()){
-            freq[ch-'a']++;
-        }
+        int max = 0; 
         
-        boolean flag = true;
-        
-        int start = 0;
-        int max = 0;
-        for(int end = 0;end<s.length();end++){
-            char ch = s.charAt(end);
-            if(freq[ch-'a']>0 && freq[ch-'a']<k){
-                String substr = s.substring(start,end);
-                max = Math.max(max,longestSubstring(substr,k));
-                flag = false;
-                start = end+1;
+        int left = 0;
+        while (left < s.length()) {
+            int[] freq = new int[26];
+            int right = left; 
+            int validLen = 0;
+            
+            while (right < s.length()) {
+                char ch = s.charAt(right);
+                freq[ch - 'a']++;
+                
+                boolean isValid = true;
+                for (int i = 0; i < 26; i++) {
+                    if (freq[i] > 0 && freq[i] < k) {
+                        isValid = false; 
+                        break;
+                    }
+                }
+                
+                if (isValid) {
+                    validLen = right - left + 1;
+                    max = Math.max(max, validLen);
+                }
+                right++;
             }
+            
+            left++;
         }
-        
-        if(flag == true){
-            return s.length();
-        }
-        else{
-            return Math.max(max,longestSubstring(s.substring(start),k));
-        }
+        return max;
     }
 }
