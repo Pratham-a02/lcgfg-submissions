ab4415f588ae0d23120cf7595c677324263bcdfa
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-11-16 10:00:25 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (95.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3254-find-the-power-of-k-size-subarrays-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3254-find-the-power-of-k-size-subarrays-i.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3254-find-the-power-of-k-size-subarrays-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,27 +1,24 @@
 class Solution{
     public int[] resultsArray(int[] nums,int k){
+        if(k == 1) return nums;
         int n = nums.length;
         int[] ans = new int[n-k+1];
         int i = 0;
         int left = 0;
-        for(int right = 0;right<n;right++){
-            int max = 0;
+        int right = 1;
+        Arrays.fill(ans,-1);
+        while(right < n){
+            if(nums[right-1]+1 != nums[right]){
+                left = right;
+            }
             if(right-left+1 == k){
-                max = Math.max(max,nums[left]);
-                int idx = left+1;
-                while(idx<=right){
-                    max = Math.max(max,nums[idx]);
-                    if(nums[idx] !=nums[idx-1]+1){
-                        ans[i] = -1;
-                        break;
-                    }
-                    idx++;
-                }
-                if(ans[i] != -1) ans[i] = max;
-                i++;
+                ans[left] = nums[right];
                 left++;
             }
+            right++;
         }
+        
+        
         return ans;
     }
 }
