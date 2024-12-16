766295561b0f668a9fd22e1a2b2004b0f75fcea4
Pratham-a02 <73159812+Pratham-a02@users.noreply.github.com>
2024-12-16 16:48:29 +0530

    Time: 3 ms (74.76%), Space: 57.4 MB (7.48%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divyandex-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1493-longest-subarray-of-1s-after-deleting-one-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divyandex-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,30 @@
+class Solution{
+    public int longestSubarray(int[] nums){
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+        
+        int left = 0;
+        
+        for(int right = 0;right<nums.length;right++){
+            if(nums[right] == 0) count++;
+            
+            if(count == 0){
+                max = Math.max(max,right-left+1);
+            }
+            else if(count == 1){
+                max = Math.max(max,right-left);
+            }
+            else if(count > 1){
+                while(left<right && count>1){
+                    if(nums[left] == 0) count--;
+                    left++;
+                }
+            }
+        }
+        
+        if(max == nums.length) return nums.length-1;
+        
+        if(max == Integer.MIN_VALUE) return 0;
+        return max;
+    }
+}
